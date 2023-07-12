d46ec9fbda6e003981c679dfab5b7c7ae221126c
vazha2000 <vaja.siqtura2000@gmail.com>
2023-07-12 19:47:48 +0400

    add responsiveness

--- src/components/Navbar/Navbar.styled.js
+++ src/components/Navbar/Navbar.styled.js
@@ -9,8 +9,11 @@ export const SNavbar = styled.nav`
   align-items: center;
   justify-content: space-between;
 
-  @media screen and (max-width: 1024px){
-    background-color: red;
+  @media screen and (max-width: 1200px){
+    padding: 0 4rem;
+  }
+  @media screen and (max-width: 968px) {
+    padding: 0 2rem;
   }
 `;
 
@@ -22,15 +25,24 @@ export const SLogo = styled.img`
 export const SNavMenu = styled.ul`
   display: flex;
   gap: 3rem;
+  padding: 0 1rem;
+  @media screen and (max-width: 968px) {
+    display: none;
+  }
 `;
 
 export const SNavItem = styled.li`
   font-size: 1rem;
   cursor: pointer;
+  display: flex;
+  align-items: center;
 `;
 
 export const SNavActions = styled.div`
   display: flex;
+  /* @media screen and (max-width: 968px) {
+    display: none;
+  } */
 `;
 
 export const SNavInputContainer = styled.div`
@@ -38,7 +50,7 @@ export const SNavInputContainer = styled.div`
   background-color: #f5f5f5;
   height: 2.375rem;
   width: 15rem;
-
+  border-radius: .25rem;
   display: flex;
 `
 
