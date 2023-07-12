faa6912267bac3c1e96605f58f9776c402ccea5c
vazha2000 <vaja.siqtura2000@gmail.com>
2023-07-12 13:48:30 +0400

    add padding to the mainPage

---
+++ src/views/MainPage/MainPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { SMainPage } from './MainPage.styled'
+
+export const MainPage = () => {
+  return (
+    <SMainPage>
+      dsdok
+    </SMainPage>
+  )
+}
---
+++ src/views/MainPage/MainPage.styled.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const SMainPage = styled.div`
+  padding: 0 8.5rem;
+`
