2723c22a4c90cb019fdea0ac63aaec9dd6ec0e4f
vazha2000 <vaja.siqtura2000@gmail.com>
2023-07-14 15:06:10 +0400

    install framer-motion library, add categories list to sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
-import { SSidebarContainer } from './Sidebar.styled'
+import { SSideBarCategories, SSideBarCategoriesList, SSidebarContainer } from './Sidebar.styled'
 
 export const Sidebar = () => {
   return (
-    <SSidebarContainer>Sidebar</SSidebarContainer>
+    <SSidebarContainer>
+      <SSideBarCategories>
+        <SSideBarCategoriesList>კომპიუტერული ტექნიკა</SSideBarCategoriesList>
+        <SSideBarCategoriesList>მსხვილი საყოფაცხოვრებო ტექნიკა</SSideBarCategoriesList>
+        <SSideBarCategoriesList>წვრილი საყოფაცხოვრებო ტექნიკა</SSideBarCategoriesList>
+        <SSideBarCategoriesList>ხელსაწყო ინსტრუმენტები</SSideBarCategoriesList>
+        <SSideBarCategoriesList>მობილური ტელეფონები</SSideBarCategoriesList>
+        <SSideBarCategoriesList>თავის მოვლის საშუალებები</SSideBarCategoriesList>
+        <SSideBarCategoriesList>სპორტი და გართობა</SSideBarCategoriesList>
+        <SSideBarCategoriesList>გათბობა და გაგრილება</SSideBarCategoriesList>
+        <SSideBarCategoriesList>საბავშვო ინვენტარი</SSideBarCategoriesList>
+        <SSideBarCategoriesList>სამზარეულოს ტექნიკა</SSideBarCategoriesList>
+        <SSideBarCategoriesList>ინტერიერი და ავეჯი</SSideBarCategoriesList>
+        <SSideBarCategoriesList>სამშენებლო მასალები</SSideBarCategoriesList>
+        <SSideBarCategoriesList>ავტომობილების განყოფილება</SSideBarCategoriesList>
+      </SSideBarCategories>
+    </SSidebarContainer>
   )
 }
--- src/components/Sidebar/Sidebar.styled.js
+++ src/components/Sidebar/Sidebar.styled.js
@@ -2,5 +2,21 @@ import styled from "styled-components";
 
 export const SSidebarContainer = styled.div`
   flex: 1;
-  background-color: green;
-`
+  /* background-color: #d5d8de; */
+`;
+
+export const SSideBarCategories = styled.ul`
+  display: flex;
+  flex-direction: column;
+  /* background-color: blue; */
+`;
+
+export const SSideBarCategoriesList = styled.li`
+  cursor: pointer;
+  padding: 10px 0;
+  font-weight: bold;
+
+  &:hover {
+    color: #3498db;
+  }
+`;
