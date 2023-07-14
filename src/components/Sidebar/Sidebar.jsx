2bfca555b3368189260123ae29387cc28b6ea1c8
vazha2000 <vaja.siqtura2000@gmail.com>
2023-07-14 17:04:57 +0400

    modify categoriesListItems

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -6,20 +6,63 @@ import {
 } from "./Sidebar.styled";
 
 const categoriesListItems = [
-  "კომპიუტერული ტექნიკა",
-  "მსხვილი საყოფაცხოვრებო ტექნიკა",
-  "წვრილი საყოფაცხოვრებო ტექნიკა",
-  "ხელსაწყო ინსტრუმენტები",
-  "მობილური ტელეფონები",
-  "თავის მოვლის საშუალებები",
-  "სპორტი და გართობა",
-  "გათბობა და გაგრილება",
-  "საბავშვო ინვენტარი",
-  "სამზარეულოს ტექნიკა",
-  "ინტერიერი და ავეჯი",
-  "სამშენებლო მასალები",
-  "ავტომობილების განყოფილება",
+  {
+    name: "კომპიუტერული ტექნიკა",
+    subcategories: ["subcategory 1", "subcategory 2", "subcategory 3"],
+  },
+  {
+    name: "მსხვილი საყოფაცხოვრებო ტექნიკა",
+    subcategories: ["subcategory 4", "subcategory 5", "subcategory 6"],
+  },
+  {
+    name: "წვრილი საყოფაცხოვრებო ტექნიკა",
+    subcategories: ["subcategory 7", "subcategory 8", "subcategory 9"],
+  },
+  {
+    name: "ხელსაწყო ინსტრუმენტები",
+    subcategories: ["subcategory 10", "subcategory 11", "subcategory 12"],
+  },
+  {
+    name: "მობილური ტელეფონები",
+
+    subcategories: ["subcategory 13", "subcategory 14", "subcategory 15"],
+  },
+  {
+    name: "თავის მოვლის საშუალებები",
+    subcategories: ["subcategory 16", "subcategory 17", "subcategory 18"],
+  },
+  {
+    name: "სპორტი და გართობა",
+    subcategories: ["subcategory 19", "subcategory 20", "subcategory 21"],
+  },
+  {
+    name: "გათბობა და გაგრილება",
+    subcategories: ["subcategory 22", "subcategory 23", "subcategory 24"],
+  },
+  {
+    name: "საბავშვო ინვენტარი",
+    subcategories: ["subcategory 25", "subcategory 26", "subcategory 27"],
+  },
+  {
+    name: "სამზარეულოს ტექნიკა",
+    subcategories: ["subcategory 28", "subcategory 29", "subcategory 30"],
+  },
+  {
+    name: "ინტერიერი და ავეჯი",
+    subcategories: ["subcategory 31", "subcategory 32", "subcategory 33"],
+  },
+  {
+    name: "სამშენებლო მასალები",
+    subcategories: ["subcategory 34", "subcategory 35", "subcategory 36"],
+  },
+  {
+    name: "ავტომობილების განყოფილება",
+    subcategories: ["subcategory 37", "subcategory 38", "subcategory 39"],
+  },
 ];
+
+console.log(categoriesListItems)
+
 export const Sidebar = () => {
   return (
     <SSidebarContainer>
@@ -27,7 +70,7 @@ export const Sidebar = () => {
         {categoriesListItems.map((item, index) => {
           return (
             <SSideBarCategoriesList key={index}>
-              <span>{item}</span>
+              <span>{item.name}</span>
               <img src="assets/svg/vectorRight.svg" alt="vectorRight" />
             </SSideBarCategoriesList>
           );
