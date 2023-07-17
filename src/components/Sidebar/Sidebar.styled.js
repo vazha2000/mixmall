b4c2222c5f065c1e95fbc515193483467c7bb8f3
vazha2000 <vaja.siqtura2000@gmail.com>
2023-07-17 14:31:09 +0400

    change font weight and font size

--- src/components/Sidebar/Sidebar.styled.js
+++ src/components/Sidebar/Sidebar.styled.js
@@ -23,7 +23,8 @@ export const SSideBarCategoriesListWrapper = styled.div`
 export const SSideBarCategoriesList = styled.li`
   cursor: pointer;
   padding: .625rem 0;
-  font-weight: bold;
+  font-weight: 500;
+  font-size: 14px;
   border-bottom: 1px solid rgba(0,0,0, 0.3);
   display: flex;
   width: 100%;
