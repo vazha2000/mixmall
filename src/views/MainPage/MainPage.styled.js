894494299c20978bbf772b7eef3ed9e302562f69
vazha2000 <vaja.siqtura2000@gmail.com>
2023-07-18 16:11:01 +0400

    add new container for search input and hamburger menu

--- src/views/MainPage/MainPage.styled.js
+++ src/views/MainPage/MainPage.styled.js
@@ -1,11 +1,17 @@
 import styled from "styled-components";
 
 export const SMainPage = styled.div`
-  /* padding: 0 8.5rem; */
   width: 82%;
   margin: 0 auto;
 
   @media screen and (max-width: 1024px){
     width: 97%;
+  }
+`
+
+export const SSearchInputHamburger = styled.div`
+  display: none;
+  @media screen and (max-width: 1024px) {
+    display: flex;
   }
 `
