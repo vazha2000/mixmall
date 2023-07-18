78aab5f4223e918bfa3f5af433c80daec9434051
vazha2000 <vaja.siqtura2000@gmail.com>
2023-07-18 16:11:49 +0400

    add new container for search input and hamburger menu

--- src/views/MainPage/MainPage.jsx
+++ src/views/MainPage/MainPage.jsx
@@ -1,13 +1,17 @@
 import React from "react";
-import { SMainPage } from "./MainPage.styled";
+import { SMainPage, SSearchInputHamburger } from "./MainPage.styled";
 import { Navbar } from "../../components/Navbar";
 import { Sidebar } from "../../components/Sidebar";
 import { AdSlider } from "../../components/AdSlider";
+import { SearchInput } from "../../components/SearchInput";
 
 export const MainPage = () => {
   return (
     <SMainPage>
       <Navbar />
+      <SSearchInputHamburger>
+        <SearchInput />
+      </SSearchInputHamburger>
       <div style={{ display: "flex", marginTop: "30px"}}>
         <Sidebar />
         <AdSlider />
