0a810a124dbf9c8e0fc266fa1d4455dd26a13ded
vazha2000 <vaja.siqtura2000@gmail.com>
2023-07-18 16:09:42 +0400

    add focus and blur logic, stylize input with search icon

--- src/components/SearchInput/SearchInput.jsx
+++ src/components/SearchInput/SearchInput.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useState } from "react";
+import { SSearchIcon, SSearchInput, SSearchInputContainer } from "./SearchInput.styled";
 
 export const SearchInput = () => {
+
+  const [isFocused, setIsFocused] = useState(false);
+
+  const handleFocus = () => {
+    setIsFocused(true)
+  }
+  const handleBlur = () => {
+    setIsFocused(false)
+  }
   return (
-    <div>SearchInput</div>
-  )
-}
+    <SSearchInputContainer isFocused={isFocused}>
+      <SSearchInput  onFocus={handleFocus} onBlur={handleBlur}/>
+      <SSearchIcon src="assets/svg/search.svg" alt="search icon" />
+    </SSearchInputContainer>
+  );
+};
