0a868c2089a4e8c5d1075b9aa45ebb69bd19c3f3
vazha2000 <vaja.siqtura2000@gmail.com>
2023-07-21 10:11:56 +0400

    change front style, background colors etc...

--- src/components/MobileMenu/MobileMenu.styled.js
+++ src/components/MobileMenu/MobileMenu.styled.js
@@ -5,7 +5,8 @@ export const SMobileMenu = styled.div`
   position: absolute;
   top: 0;
   left: 0;
-  background-color: red;
+  background-color: #e3e2de;
+  /* background-color: #f2f2f2; */
   height: 100%;
   width: 70%;
   z-index: 2;
@@ -22,16 +23,24 @@ export const STitleCloseButton = styled.div`
   white-space: nowrap;
 `
 export const SMobileMenuTitle = styled.h3`
-  color: yellow;
+  color: #df3d41;
   line-height: 1;
+`
+
+export const SMobileMenuClose = styled.img`
+  cursor: pointer;
 `
 
 export const SMobileMenuCategories = styled(SSideBarCategories)`
   border-radius: 0;
+  background-color: #e3e2de;
+  box-shadow: none;
 `
 export const SMobileMenuCategoriesList = styled(SSideBarCategoriesList)`
   display: flex;
   align-items: center;
+  font-size: 13px;
+  text-shadow:1px 1px 2px rgba(0,0,0, 0.15);
   &:nth-last-child(-n+3) {
     display: none;
     @media screen and (max-width: 768px){
@@ -39,4 +48,33 @@ export const SMobileMenuCategoriesList = styled(SSideBarCategoriesList)`
     }
   }
 `
-// 768px
+export const SMobileMenuCategoriesContainer = styled.ul`
+  /* background-color: yellow; */
+`
+
+export const SMobileMenuCategoriesListItems = styled.li`
+  font-size: 12px;
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
+  &:hover {
+    background-color: aquamarine;
+    cursor: pointer;
+    /* background-color: #df3d41; */
+    background-color: rgba(223, 61, 65, 0.4);
+    color: #f2f2f2;
+  }
+`
+
+export const SMobileSubcategoriesList = styled.ul`
+  padding: 0 .8rem;
+`
+
+export const SMobileSubcategoriesListItems = styled.li`
+  font-size: 13px;
+  cursor: pointer;
+  &:hover {
+    background-color: red;
+  }
+`
