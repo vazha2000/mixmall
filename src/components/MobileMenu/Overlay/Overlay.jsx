126d84e15d57abca182c28cdc34a7c48e5ad0e27
vazha2000 <vaja.siqtura2000@gmail.com>
2023-07-21 13:50:40 +0400

    add hidden and visible animations to overlay component

--- src/components/MobileMenu/Overlay/Overlay.jsx
+++ src/components/MobileMenu/Overlay/Overlay.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 import { SOverlay } from './Overlay.styled'
 
 export const Overlay = ({isClicked, handleMobileMenuClick}) => {
+  const overlayVariants = {
+    hidden: { opacity: 0},
+    visible: {opacity: 1}
+  }
   return (
-    <SOverlay isClicked={isClicked}  onClick={handleMobileMenuClick}/>
+    <SOverlay
+    onClick={handleMobileMenuClick}
+    initial="hidden"
+    animate={isClicked ? "visible" : "hidden"}
+    exit="hidden"
+    variants={overlayVariants}
+    />
   )
 }
