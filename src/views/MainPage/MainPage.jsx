b67b2ec99c0dd9f71dc69827654a8b9f5138d4b5
vazha2000 <vaja.siqtura2000@gmail.com>
2023-07-21 10:12:48 +0400

    pass down handleMobileMenuClick as prop to MobileMenu component

--- src/views/MainPage/MainPage.jsx
+++ src/views/MainPage/MainPage.jsx
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { SBurgerMenuIcon, SMainPage, SSearchInputHamburger } from "./MainPage.styled";
 import { Navbar } from "../../components/Navbar";
 import { Sidebar } from "../../components/Sidebar";
 import { AdSlider } from "../../components/AdSlider";
 import { SearchInput } from "../../components/SearchInput";
+import { MobileMenu } from "../../components/MobileMenu";
 
 export const MainPage = () => {
+  const [isClicked, setIsClicked] = useState(false)
+
+  const handleMobileMenuClick = () => {
+    setIsClicked(!isClicked)
+  }
   return (
     <SMainPage>
       <Navbar />
       <SSearchInputHamburger>
-        <SBurgerMenuIcon width={40} src="assets/svg/burgerMenu.svg" alt="menu"/>
+        <SBurgerMenuIcon onClick={handleMobileMenuClick}  width={40} src="assets/svg/burgerMenu.svg" alt="menu"/>
         <SearchInput />
+        {isClicked && <MobileMenu handleMobileMenuClick={handleMobileMenuClick}/>}
       </SSearchInputHamburger>
       <div style={{ display: "flex", marginTop: "30px"}}>
         <Sidebar />
