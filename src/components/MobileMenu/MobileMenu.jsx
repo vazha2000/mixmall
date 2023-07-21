91ca09e81957a8a78fbb70c9f12c839ab89dcb5b
vazha2000 <vaja.siqtura2000@gmail.com>
2023-07-21 09:21:56 +0400

    add subcategories opening&closing controls which work independently

--- src/components/MobileMenu/MobileMenu.jsx
+++ src/components/MobileMenu/MobileMenu.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
-import { SMobileMenu, SMobileMenuCategories, SMobileMenuCategoriesList, SMobileMenuTitle, STitleCloseButton } from './MobileMenu.styled'
+import React from "react";
+import {
+  SMobileMenu,
+  SMobileMenuCategories,
+  SMobileMenuCategoriesContainer,
+  SMobileMenuCategoriesList,
+  SMobileMenuCategoriesListItems,
+  SMobileMenuTitle,
+  SMobileSubcategoriesList,
+  STitleCloseButton,
+} from "./MobileMenu.styled";
+import { useState } from "react";
+import { categoriesListItems } from "../../data/data";
 
 export const MobileMenu = () => {
+  const [isMenuClicked, setIsMenuClicked] = useState(false);
+  const [openSubcategories, setOpenSubcategories] = useState(
+    Array(categoriesListItems.length).fill(false)
+  );
+
+  const handleCategoryClick = (index) => {
+    const newOpenSubcategories = [...openSubcategories];
+    newOpenSubcategories[index] = !newOpenSubcategories[index];
+    setOpenSubcategories(newOpenSubcategories)
+  };
+
   return (
     <SMobileMenu>
       <STitleCloseButton>
@@ -9,14 +31,45 @@ export const MobileMenu = () => {
         <img src="assets/svg/close.svg" alt="close icon" />
       </STitleCloseButton>
       <SMobileMenuCategories>
-        <SMobileMenuCategoriesList>
+        <SMobileMenuCategoriesList
+          onClick={() => setIsMenuClicked(!isMenuClicked)}
+        >
           <span>კატეგორიები</span>
           <img src="assets/svg/vectorDown.svg" alt="vector down" />
         </SMobileMenuCategoriesList>
+        {isMenuClicked && (
+          <SMobileMenuCategoriesContainer>
+            {categoriesListItems.map((item, index) => {
+              return (
+                <React.Fragment key={index}>
+                  <SMobileMenuCategoriesListItems
+                    onClick={() => handleCategoryClick(index)}
+                  >
+                    <span>{item.name}</span>
+                    <img
+                      src="assets/svg/vectorDown.svg"
+                      width={25}
+                      alt="vector down"
+                    />
+                  </SMobileMenuCategoriesListItems>
+                  {openSubcategories[index] && (
+                    <SMobileSubcategoriesList>
+                      {item.subcategories.map((subcategory, subIndex) => (
+                        <SMobileMenuCategoriesListItems key={subIndex}>
+                          {subcategory}
+                        </SMobileMenuCategoriesListItems>
+                      ))}
+                    </SMobileSubcategoriesList>
+                  )}
+                </React.Fragment>
+              );
+            })}
+          </SMobileMenuCategoriesContainer>
+        )}
         <SMobileMenuCategoriesList>მთავარი</SMobileMenuCategoriesList>
         <SMobileMenuCategoriesList>ONLINE განვადება</SMobileMenuCategoriesList>
         <SMobileMenuCategoriesList>კონტაქტი</SMobileMenuCategoriesList>
       </SMobileMenuCategories>
     </SMobileMenu>
-  )
-}
+  );
+};
