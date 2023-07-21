f11b3b780b8d6dc4f6302bed1aa0bacd3ee18365
vazha2000 <vaja.siqtura2000@gmail.com>
2023-07-21 08:08:04 +0400

    add mobile menu structure

--- src/components/MobileMenu/MobileMenu.jsx
+++ src/components/MobileMenu/MobileMenu.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import { SMobileMenu, SMobileMenuCategories, SMobileMenuCategoriesList, SMobileMenuTitle, STitleCloseButton } from './MobileMenu.styled'
 
 export const MobileMenu = () => {
   return (
-    <div>MobileMenu</div>
+    <SMobileMenu>
+      <STitleCloseButton>
+        <SMobileMenuTitle>კატეგორიები</SMobileMenuTitle>
+        <img src="assets/svg/close.svg" alt="close icon" />
+      </STitleCloseButton>
+      <SMobileMenuCategories>
+        <SMobileMenuCategoriesList>
+          <span>კატეგორიები</span>
+          <img src="assets/svg/vectorDown.svg" alt="vector down" />
+        </SMobileMenuCategoriesList>
+        <SMobileMenuCategoriesList>მთავარი</SMobileMenuCategoriesList>
+        <SMobileMenuCategoriesList>ONLINE განვადება</SMobileMenuCategoriesList>
+        <SMobileMenuCategoriesList>კონტაქტი</SMobileMenuCategoriesList>
+      </SMobileMenuCategories>
+    </SMobileMenu>
   )
 }
