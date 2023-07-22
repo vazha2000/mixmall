4a5cdef69eb0a47b7e5a9126f6133345c0597eb8
vazha2000 <vaja.siqtura2000@gmail.com>
2023-07-22 17:35:52 +0400

    update object data

--- src/data/data.js
+++ src/data/data.js
@@ -3,65 +3,91 @@ export const categoriesListItems = [
     id: 1,
     name: "კომპიუტერული ტექნიკა",
     subcategories: ["subcategory 1", "subcategory 2", "subcategory 3"],
+    svg: "assets/svg/CategoriesIcons/computer.svg",
+    alt: "computer"
   },
   {
     id: 2,
     name: "მსხვილი საყოფაცხოვრებო ტექნიკა",
     subcategories: ["subcategory 4", "subcategory 5", "subcategory 6"],
+    svg: "assets/svg/CategoriesIcons/washingmachine.svg",
+    alt: "washingmachine"
   },
   {
     id: 3,
     name: "წვრილი საყოფაცხოვრებო ტექნიკა",
     subcategories: ["subcategory 7", "subcategory 8", "subcategory 9"],
+    svg: "assets/svg/CategoriesIcons/iron.svg",
+    alt: "iron"
   },
   {
     id: 4,
     name: "ხელსაწყო ინსტრუმენტები",
     subcategories: ["subcategory 10", "subcategory 11", "subcategory 12"],
+    svg: "assets/svg/CategoriesIcons/tools.svg",
+    alt: "tools"
   },
   {
     id: 5,
     name: "მობილური ტელეფონები",
     subcategories: ["subcategory 13", "subcategory 14", "subcategory 15"],
+    svg: "assets/svg/CategoriesIcons/mobile.svg",
+    alt: "mobile"
   },
   {
     id: 6,
     name: "თავის მოვლის საშუალებები",
     subcategories: ["subcategory 16", "subcategory 17", "subcategory 18"],
+    svg: "assets/svg/CategoriesIcons/selfcare.svg",
+    alt: "self-care"
   },
   {
     id: 7,
     name: "სპორტი და გართობა",
     subcategories: ["subcategory 19", "subcategory 20", "subcategory 21"],
+    svg: "assets/svg/CategoriesIcons/sports.svg",
+    alt: "sports"
   },
   {
     id: 8,
     name: "გათბობა და გაგრილება",
     subcategories: ["subcategory 22", "subcategory 23", "subcategory 24"],
+    svg: "assets/svg/CategoriesIcons/conditioner.svg",
+    alt: "air conditioner"
   },
   {
     id: 9,
     name: "საბავშვო ინვენტარი",
     subcategories: ["subcategory 25", "subcategory 26", "subcategory 27"],
+    svg: "assets/svg/CategoriesIcons/toys.svg",
+    alt: "toys"
   },
   {
     id: 10,
     name: "სამზარეულოს ტექნიკა",
     subcategories: ["subcategory 28", "subcategory 29", "subcategory 30"],
+    svg: "assets/svg/CategoriesIcons//kitchen.svg",
+    alt: "kitchen"
   },
   {
     id: 11,
     name: "ინტერიერი და ავეჯი",
     subcategories: ["subcategory 31", "subcategory 32", "subcategory 33"],
+    svg: "assets/svg/CategoriesIcons/furniture.svg",
+    alt: "furniture"
   },
   {
     id: 12,
     name: "სამშენებლო მასალები",
     subcategories: ["subcategory 34", "subcategory 35", "subcategory 36"],
+    svg: "assets/svg/CategoriesIcons/buildingTools.svg",
+    alt: "building tools"
   },
   {
     id: 13,
     name: "ავტომობილების განყოფილება",
     subcategories: ["subcategory 37", "subcategory 38", "subcategory 39"],
+    svg: "assets/svg/CategoriesIcons/car.svg",
+    alt: "car"
   },
 ];
