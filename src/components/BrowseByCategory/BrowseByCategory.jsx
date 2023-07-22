68f78de2e237ca56366c628bd8c3d52b4e1703a9
vazha2000 <vaja.siqtura2000@gmail.com>
2023-07-22 15:04:06 +0400

    add custom arrows and change the style

--- src/components/BrowseByCategory/BrowseByCategory.jsx
+++ src/components/BrowseByCategory/BrowseByCategory.jsx
@@ -1,7 +1,79 @@
-import React from 'react'
+import React from "react";
+import {
+  SBrowseByCategory,
+  SBrowseByCategoryBox,
+  SBrowseByCategoryHeader,
+  SBrowseByCategoryHeaderSlider,
+  SBrowseByCategoryLabel,
+  SBrowseByCategoryLabelText,
+  SBrowseByCategorySlider,
+} from "./BrowseByCategory.styled";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import styled from "styled-components";
+import { CustomNextArrow, CustomPrevArrow } from "./CustomArrows/CustomArrows";
+
+const PrevArrow = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 50px;
+  height: 50px;
+  background-color: red;
+`
+
+ const NextArrow = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 50px;
+  height: 50px;
+  background-color: red;
+`
 
 export const BrowseByCategory = () => {
+  const settings = {
+    dots: false,
+    arrows: true,
+    infinite: true,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    autoplay: false,
+    swipeToSlide: true,
+    speed: 200,   
+    PrevArrow: <CustomPrevArrow />,
+    NextArrow: <CustomNextArrow />
+  };
   return (
-    <div>BrowseByCategory</div>
-  )
-}
+    <SBrowseByCategory>
+      <SBrowseByCategoryLabel>
+        <img src="assets/svg/labelIcon.svg" alt="label icon" />
+        <SBrowseByCategoryLabelText>კატეგორიები</SBrowseByCategoryLabelText>
+      </SBrowseByCategoryLabel>
+      <SBrowseByCategoryHeaderSlider>
+        <SBrowseByCategoryHeader>
+          მოძებნე კატეგორიების მიხედვით
+        </SBrowseByCategoryHeader>
+        <SBrowseByCategorySlider>
+          <Slider {...settings} prevArrow={<CustomPrevArrow />} nextArrow={<CustomNextArrow />}>
+            <SBrowseByCategoryBox>1. gela</SBrowseByCategoryBox>
+            <SBrowseByCategoryBox>2. bnela</SBrowseByCategoryBox>
+            <SBrowseByCategoryBox>3. cxela</SBrowseByCategoryBox>
+            <SBrowseByCategoryBox>4. aba raa</SBrowseByCategoryBox>
+            <SBrowseByCategoryBox>5. modiii</SBrowseByCategoryBox>
+            <SBrowseByCategoryBox>6. mausi</SBrowseByCategoryBox>
+            <SBrowseByCategoryBox>7. keyboard</SBrowseByCategoryBox>
+            <SBrowseByCategoryBox>8. modiii</SBrowseByCategoryBox>
+            <SBrowseByCategoryBox>9. modiii</SBrowseByCategoryBox>
+            <SBrowseByCategoryBox>10. modiii</SBrowseByCategoryBox>
+            <SBrowseByCategoryBox>11. modiii</SBrowseByCategoryBox>
+            <SBrowseByCategoryBox>12. modiii</SBrowseByCategoryBox>
+            <SBrowseByCategoryBox>13. modiii</SBrowseByCategoryBox>
+          </Slider>
+        </SBrowseByCategorySlider>
+      </SBrowseByCategoryHeaderSlider>
+    </SBrowseByCategory>
+  );
+};
---
+++ src/components/BrowseByCategory/BrowseByCategory.styled.js
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+
+export const SBrowseByCategory = styled.div`
+  width: 100%;
+  margin-top: 5rem;
+  /* border: 1px solid red; */
+  height: 500px;
+  position: relative;
+
+`
+
+export const SBrowseByCategoryLabel = styled.label`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+`
+
+export const SBrowseByCategoryLabelText = styled.span`
+  color: #df3d41;
+  font-weight: bold;
+  text-shadow: 0px 0px 0px red;
+`
+export const SBrowseByCategoryHeaderSlider = styled.div`
+  /* background-color: red; */
+  margin-top: 1.25rem;
+`
+export const SBrowseByCategoryHeader = styled.h2`
+  margin-bottom: 3rem;
+
+`
+
+export const SBrowseByCategorySlider = styled.div`
+  .slick-slide {
+    display: flex;
+    justify-content: center;
+  }
+`
+
+export const SBrowseByCategoryBox = styled.div`
+  border: 1px solid rgba(0,0,0, 0.3);
+  border-radius: 4px;
+  height: 145px;
+  width: 170px !important;
+`
+
+
+
