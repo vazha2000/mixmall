f61f72403bc168457dc3e83ef8429d4ebb6b03f4
vazha2000 <vaja.siqtura2000@gmail.com>
2023-07-23 15:52:37 +0400

    update cards container and delete unnecessary div

--- src/components/SaleOfMonth/SaleOfMonth.styled.js
+++ src/components/SaleOfMonth/SaleOfMonth.styled.js
@@ -41,66 +41,8 @@ export const SSaleOfMonthItemsContainer = styled.div``;
 
 export const SSaleOfMonthItems = styled.div`
   margin-top: 60px;
+  row-gap: 40px;
   display: flex;
   flex-wrap: wrap;
-  /* background-color: red; */
   justify-content: space-between;
-  row-gap: 20px;
 `;
-
-export const SSaleOfMonthItemsCard = styled.div`
-  width: 250px;
-  height: auto;
-  border: 1px solid rgba(0,0,0, 0.3);
-  border-radius: 4px;
-  padding-bottom: 10px;
-  /* cursor: pointer; */
-`;
-
-export const SSaleOfMonthItemsCardImageContainer = styled.div`
-  width: 100%;
-  height: 250px;
-  border-radius: 5px 5px 0 0;
-  background-color: #e8e8e8;
-  position: relative;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-export const SSaleOfMonthItemsCardImage = styled.img`
-  width: 180px;
-`
-
-export const SSaleOfMonthWishlist = styled.img`
-  position: absolute;
-  top: 10px;
-  right: 10px;
-  background-color: #f2f2f2;
-  border-radius: 50%;
-  padding: 5px;
-`
-
-export const SSaleOfMonthItemsCardInfo = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-
-export const SSaleOfMonthItemsCardName = styled.h4`
-  margin: 1rem 0;
-  font-size: 14px;
-  text-shadow: 0px 0px 0px black;
-
-`
-export const SSaleOfMonthItemsCardPriceContainer = styled.div`
-  display: flex;
-  gap: 20px;
-`
-
-export const SSaleOfMonthItemsCardPrice = styled.span`
-  color: #df3d41;
-`
-
-export const SSaleOfMonthItemsCardOldPrice = styled.span`
-  text-decoration: line-through;
-`
