40a4949dc09631ca68c924e32225ca1a0b6c0f1e
vazha2000 <vaja.siqtura2000@gmail.com>
2023-07-24 13:41:55 +0400

    add handleHover functionality

--- src/components/BestSellingProducts/BestSellingProducts.jsx
+++ src/components/BestSellingProducts/BestSellingProducts.jsx
@@ -1,14 +1,43 @@
-import React from 'react'
-import { SBestSellingProducts, SBestSellingProductsLabel, SBestSellingProductsLabelText } from './BestSellingProducts.styled'
+import React, { useState } from 'react'
+import { SBestSellingProducts, SBestSellingProductsHeader, SBestSellingProductsItems, SBestSellingProductsLabel, SBestSellingProductsLabelText } from './BestSellingProducts.styled'
+import { testProducts } from '../../data/data'
+import { Card } from '../Card'
 
 export const BestSellingProducts = () => {
+
+  const [hoverStates, setHoverStates] = useState(testProducts.map(() => false));
+
+  const handleHover = (index) => {
+    setHoverStates((prevStates) => {
+      const updatedStates = [...prevStates];
+      updatedStates[index] = !updatedStates[index];
+      return updatedStates
+    })
+  }
+
   return (
     <SBestSellingProducts>
       <SBestSellingProductsLabel>
         <img src="assets/svg/labelIcon.svg" alt="label icon" />
         <SBestSellingProductsLabelText>ამ თვეში</SBestSellingProductsLabelText>
       </SBestSellingProductsLabel>
-
+      <SBestSellingProductsHeader>საუკეთესოდ გაყიდვადი პროდუქცია</SBestSellingProductsHeader>
+      <SBestSellingProductsItems>
+      {testProducts.map((item, index) => {
+          return <Card
+          key={index}
+          productImage={item.productImage}
+          discountRate={item.discountRate}
+          isDiscount={item.isDiscount}
+          productName={item.productName}
+          currentPrice={item.currentPrice}
+          oldPrice={item.oldPrice}
+          alt={item.alt}
+          handleHover={() => handleHover(index)}
+          isHovered={hoverStates[index]}
+        />
+        })}
+      </SBestSellingProductsItems>
     </SBestSellingProducts>
   )
 }
