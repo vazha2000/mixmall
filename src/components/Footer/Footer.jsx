2a3a275f5b0ed47cdd25b480595e49ae3c7d4300
vazha2000 <vaja.siqtura2000@gmail.com>
2023-07-25 15:08:06 +0400

    add footer components; add style

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,31 @@
 import React from 'react'
+import { SFooter, SFooterContact, SFooterInfo, SFooterLogo, SFooterLogoContainer, SFooterLogoSocial, SFooterSocial, SFooterSocialContainer } from './Footer.styled'
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <SFooter>
+      <SFooterLogoSocial>
+        <SFooterLogoContainer>
+          <SFooterLogo src='assets/images/LogoDark.png'/>
+        </SFooterLogoContainer>
+        <SFooterSocialContainer>
+          <SFooterSocial src='assets/svg/Footer/fb.svg'/>
+          <SFooterSocial src='assets/svg/Footer/instagram.svg'/>
+          <SFooterSocial src='assets/svg/Footer/tiktok.svg'/>
+        </SFooterSocialContainer>
+      </SFooterLogoSocial>
+      <SFooterInfo>
+        <h4>საინფორმაციო</h4>
+        <span>ჩვენ შესახებ</span>
+        <span>საგარანტიო პირობები</span>
+        <span>მიწოდების პირობები</span>
+        <span>კონფიდენციალურობა</span>
+      </SFooterInfo>
+      <SFooterContact>
+        <h4>კონტაქტი</h4>
+        <span>579-33-33-11</span>
+        <span>mixmall.ge@gmail.com</span>
+      </SFooterContact>
+    </SFooter>
   )
 }
