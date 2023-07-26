09c78a6391cf1a3e35758b056b1252533790220f
vazha2000 <vaja.siqtura2000@gmail.com>
2023-07-26 18:18:36 +0400

    add temporary style

---
+++ src/views/SubCategories/Subcategories.styled.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const SSubcategories = styled.div`
+  
+`
+export const SSubcategoriesBreadcrumbs = styled.div`
+  background-color: red;
+`
+
+export const SSidebarContainer = styled.div`
+  flex: 1;
+`
+
+export const SSubcategoriesSidebarContainer = styled.div`
+  background-color: yellow;
+  flex: 1;
+`
+
+export const SSubcategoriesProducts = styled.ul`
+  /* background-color: blue; */
+  flex: 3;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  row-gap: 20px;
+`
