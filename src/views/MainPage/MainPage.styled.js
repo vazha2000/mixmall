cb5b9b6d22144bcb9c7267e25b428598cd115341
vazha2000 <vaja.siqtura2000@gmail.com>
2023-07-26 14:23:42 +0400

    remove mainpage style

--- src/views/MainPage/MainPage.styled.js
+++ src/views/MainPage/MainPage.styled.js
@@ -1,12 +1,12 @@
 import styled from "styled-components";
 
 export const SMainPage = styled.div`
-  width: 84%;
+  /* width: 84%;
   margin: 0 auto;
 
   @media screen and (max-width: 1024px){
     width: 97%;
-  }
+  } */
 `
 
 export const SSidebarAndSlider = styled.div`
