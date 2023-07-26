90135c32887b347b1c520faae2914d558dea688b
vazha2000 <vaja.siqtura2000@gmail.com>
2023-07-26 18:18:22 +0400

    apply card component for all products

--- src/views/SubCategories/Subcategories.jsx
+++ src/views/SubCategories/Subcategories.jsx
@@ -1,7 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Sidebar } from '../../components/Sidebar'
+import { SSubcategories, SSubcategoriesBreadcrumbs, SSubcategoriesProducts, SSubcategoriesSidebarContainer } from './Subcategories.styled'
+import { Card } from '../../components/Card'
+import { categoriesListItems, testProducts } from '../../data/data'
 
-export const Subcategories = () => {
+export const Subcategories = ({item}) => {
+  const [hoverStates, setHoverStates] = useState(testProducts.map(() => false));
+
+  const handleHover = (index) => {
+    setHoverStates((prevStates) => {
+      const updatedStates = [...prevStates];
+      updatedStates[index] = !updatedStates[index];
+      return updatedStates
+    })
+  }
+
+  console.log(item.products)
   return (
-    <div>Subcategories</div>
+    <SSubcategories>
+      <SSubcategoriesBreadcrumbs>
+        {item.name}
+      </SSubcategoriesBreadcrumbs>
+      <div style={{display: "flex"}}>
+        <SSubcategoriesSidebarContainer>
+          <Sidebar />
+        </SSubcategoriesSidebarContainer>
+        <SSubcategoriesProducts>
+          {/* {testProducts.map((card, index) => {
+            return <Card key={index}
+            productImage={card.productImage}
+            discountRate={card.discountRate}
+            isDiscount={card.isDiscount}
+            productName={card.productName}
+            currentPrice={card.currentPrice}
+            oldPrice={card.oldPrice}
+            alt={card.alt}
+            handleHover={() => handleHover(index)}
+            isHovered={hoverStates[index]}
+            />
+
+          })} */}
+          {item.products.map((card, index) => {
+            return <Card 
+            key={index}
+            productImage={card.productImage}
+            discountRate={card.discountRate}
+            isDiscount={card.isDiscount}
+            productName={card.productName}
+            currentPrice={card.currentPrice}
+            oldPrice={card.oldPrice}
+            alt={card.alt}
+            handleHover={() => handleHover(index)}
+            isHovered={hoverStates[index]}
+            />
+          })}
+        </SSubcategoriesProducts>
+      </div>
+    </SSubcategories>
   )
 }
