99a8b2d0d18bb65f02aed1102a0653490b1882b8
vazha2000 <vaja.siqtura2000@gmail.com>
2023-07-27 14:56:53 +0400

    pase code from MainPage.styled

---
+++ src/components/SearchInputHamburger/SearchInputHamburger.styled.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const SBurgerMenuIcon = styled.img`
+  cursor: pointer;
+  margin-right: 1.25rem;
+`
+
+export const SSearchInputHamburger = styled.div`
+  display: none;
+  @media screen and (max-width: 1024px) {
+    display: flex;
+    margin-top: .5rem;
+  }
+`
