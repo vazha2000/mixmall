1a6de923c05c0f3e550e2ca0e213d9aa9e3c9d8f
vazha2000 <vaja.siqtura2000@gmail.com>
2023-07-27 14:57:20 +0400

    remove unnecessary code

--- src/views/MainPage/MainPage.jsx
+++ src/views/MainPage/MainPage.jsx
@@ -1,66 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import {
-  SBurgerMenuIcon,
   SMainPage,
-  SSearchInputHamburger,
   SSidebarAndSlider,
 } from "./MainPage.styled";
-import { Navbar } from "../../components/Navbar";
 import { Sidebar } from "../../components/Sidebar";
 import { AdSlider } from "../../components/AdSlider";
-import { SearchInput } from "../../components/SearchInput";
-import { MobileMenu } from "../../components/MobileMenu";
-import { AnimatePresence } from "framer-motion";
-import { Overlay } from "../../components/MobileMenu/Overlay/Overlay";
 import { BrowseByCategory } from "../../components/BrowseByCategory";
 import { SaleOfMonth } from "../../components/SaleOfMonth";
 import { BestSellingProducts } from "../../components/BestSellingProducts"
-import { Footer } from "../../components/Footer";
-import { Layout } from "../../components/Layout/Layout";
+import { SearchInputHamburger } from "../../components/SearchInputHamburger";
 
 export const MainPage = () => {
-  const [isClicked, setIsClicked] = useState(false);
-
-  const handleMobileMenuClick = () => {
-    setIsClicked(!isClicked);
-  };
-
-  const autoCloseMobileMenu = () => {
-    if (window.innerWidth > 1024 && isClicked) {
-      setIsClicked(false);
-    }
-  };
-
-  useEffect(() => {
-    window.addEventListener("resize", autoCloseMobileMenu);
-
-    return () => {
-      window.removeEventListener("resize", autoCloseMobileMenu);
-    };
-  }, [isClicked]);
-
+  
   return (
     <SMainPage>
-      <SSearchInputHamburger>
-        <SBurgerMenuIcon
-          onClick={handleMobileMenuClick}
-          width={40}
-          src="assets/svg/burgerMenu.svg"
-          alt="menu"
-        />
-        <SearchInput />
-        <AnimatePresence>
-          {isClicked && (
-            <>
-              <Overlay
-                isClicked={isClicked}
-                handleMobileMenuClick={handleMobileMenuClick}
-              />
-              <MobileMenu handleMobileMenuClick={handleMobileMenuClick} />
-            </>
-          )}
-        </AnimatePresence>
-      </SSearchInputHamburger>
+      <SearchInputHamburger />
       <SSidebarAndSlider>
         <Sidebar />
         <AdSlider />
