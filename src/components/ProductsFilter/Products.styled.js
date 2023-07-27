3a553126299520d7ddd8f754a35f595592249bc6
vazha2000 <vaja.siqtura2000@gmail.com>
2023-07-27 18:37:10 +0400

    add style

--- src/components/ProductsFilter/Products.styled.js
+++ src/components/ProductsFilter/Products.styled.js
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import styled from "styled-components";
 
 export const SProductsFilter = styled.div`
@@ -14,4 +15,51 @@ export const SProductsFilterHeaderWrapper = styled.div`
   }
   border-bottom: 1px solid rgba(0,0,0, 0.2);
   padding: 10px;
+`
+
+export const SPriceFilter = styled.div`
+  margin: 1rem 0;
+`
+
+export const SPriceFilterRange = styled.input`
+  width: 100%;
+`
+
+export const SPriceFilterInputsContainer = styled.div`
+  display: flex;
+  width: 100%;
+  gap: 1rem;
+  padding: 10px;
+  flex-wrap: wrap;
+`
+
+export const SPriceFilterInput = styled.input`
+  width: 80px;
+  height: 45px;
+  border-radius: 12px;
+  border: 1px solid rgba(0,0,0, 0.2);
+  font-size: 18px;
+  padding: 0 10px;
+
+  &:focus {
+    outline: none;
+    box-shadow: 0px 0px 15px 1px rgba(0,0,0,0.12);
+    -webkit-box-shadow: 0px 0px 15px 1px rgba(0,0,0,0.12);
+    -moz-box-shadow: 0px 0px 15px 1px rgba(0,0,0,0.12);
+  }
+`
+
+export const SPriceFilterButton = styled(motion.button)`
+  border: none;
+  background-color: #df3d41;
+  border-radius: 5px;
+  color: #f2f2f2;
+  font-size: 14px;
+  padding: 10px 18px;
+  font-family: inherit;
+
+  &:hover {
+    background-color: #c93034;
+    cursor: pointer;
+  }
 `
