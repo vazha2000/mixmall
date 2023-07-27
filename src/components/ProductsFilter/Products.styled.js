e1f8a4e206d572753014b109b3639564d530a53f
vazha2000 <vaja.siqtura2000@gmail.com>
2023-07-27 17:33:31 +0400

    add ProductsFilter folder with its file structure

---
+++ src/components/ProductsFilter/Products.styled.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const SProductsFilter = styled.div`
+  width: 95%;
+  margin-top: .8rem;
+  border: 1px solid rgba(0,0,0, 0.2);
+  border-radius: 10px;
+`
+
+export const SProductsFilterHeaderWrapper = styled.div`
+  > h2 {
+    color: black;
+    font-weight: normal;
+  }
+  border-bottom: 1px solid rgba(0,0,0, 0.2);
+  padding: 10px;
+`
---
+++ src/components/ProductsFilter/ProductsFilter.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SProductsFilter, SProductsFilterHeaderWrapper } from './Products.styled'
+
+export const ProductsFilter = () => {
+  return (
+    <SProductsFilter>
+      <SProductsFilterHeaderWrapper>
+        <h2>გაფილტრე</h2>
+      </SProductsFilterHeaderWrapper>
+    </SProductsFilter>
+  )
+}
