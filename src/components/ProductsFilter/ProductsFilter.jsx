a4bc858573fd47c40cedbd0a41ee1e72d71758fa
vazha2000 <vaja.siqtura2000@gmail.com>
2023-07-28 19:45:25 +0400

    add double range handle; keet track of range values

--- src/components/ProductsFilter/ProductsFilter.jsx
+++ src/components/ProductsFilter/ProductsFilter.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   SPriceFilter,
   SPriceFilterButton,
@@ -6,25 +6,70 @@ import {
   SPriceFilterInputRangeValue,
   SPriceFilterInputRangeValueContainer,
   SPriceFilterInputsContainer,
-  SPriceFilterRange,
+  SPriceFilterRangeBackground,
+  SPriceFilterRangeContainer,
+  SPriceFilterRangeThumb,
+  // SPriceFilterRange,
   SProductsFilter,
   SProductsFilterHeaderWrapper,
 } from "./ProductsFilter.styled";
+import { Range, getTrackBackground } from "react-range";
 
 export const ProductsFilter = () => {
+  const [values, setValues] = useState([0, 1000]);
+  const STEP = 1;
+  const MIN = 0; 
+  const MAX = 1000; 
+
   return (
     <SProductsFilter>
       <SProductsFilterHeaderWrapper>
         <h2>გაფილტრე</h2>
       </SProductsFilterHeaderWrapper>
       <SPriceFilterInputRangeValueContainer>
-        <SPriceFilterInputRangeValue>100 - 200</SPriceFilterInputRangeValue>
+        <SPriceFilterInputRangeValue>{values[0]}$ - {values[1]}$</SPriceFilterInputRangeValue>
       </SPriceFilterInputRangeValueContainer>
       <SPriceFilter>
-        <SPriceFilterRange type="range" />
+        <Range
+          values={values}
+          step={STEP}
+          min={MIN}
+          max={MAX}
+          onChange={(values) => setValues(values)}
+          renderTrack={({ props, children }) => (
+            <SPriceFilterRangeContainer>
+              <SPriceFilterRangeBackground
+              {...props}
+              style={{
+                ...props.style,
+                background: getTrackBackground({
+                  values,
+                  colors: ["#ccc", "#df3d41", "#ccc"],
+                  min: MIN,
+                  max: MAX,
+                }),
+              }}
+            >
+              {children}
+            </SPriceFilterRangeBackground>
+            </SPriceFilterRangeContainer>
+            
+          )}
+          renderThumb={({ props }) => (
+            <SPriceFilterRangeThumb
+              {...props}
+              style={{
+                ...props.style,
+                cursor: "pointer"
+              }}
+            ></SPriceFilterRangeThumb>
+          )}
+        />
         <SPriceFilterInputsContainer>
-          <SPriceFilterInput type="text" />
-          <SPriceFilterInput type="text" />
+          {/* <SPriceFilterInput type="text" value={values[0]} onChange={(e) => setValues([parseInt(e.target.value), values[1]])} />
+          <SPriceFilterInput type="text" value={values[1]} onChange={(e) => setValues([values[0], parseInt(e.target.value)])}/> */}
+          <SPriceFilterInput type="text"/>
+          <SPriceFilterInput type="text"/>
           <SPriceFilterButton whileTap={{ scale: 0.98 }}>
             გაფილტრე
           </SPriceFilterButton>
