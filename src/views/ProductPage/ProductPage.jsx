5f231c768c666eaa1e1fbbab241b7f2b43a7299d
vazha2000 <vaja.siqtura2000@gmail.com>
2023-08-01 12:21:46 +0400

    add image componentt; add product options

--- src/views/ProductPage/ProductPage.jsx
+++ src/views/ProductPage/ProductPage.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import { SProductPage, SProductPageBigImage, SProductPageImages, SProductPageMainImage, SProductPageOptions, SProductPageSmallImage, SProductPageSmallImages } from './ProductPage.styled'
 
-export const ProductPage = () => {
+export const ProductPage = ({product}) => {
+  console.log(product)
   return (
-    <div>ProductPage</div>
+    <SProductPage>
+      <SProductPageImages>
+        <SProductPageSmallImages>
+          <SProductPageSmallImage src="assets/images/computerTechnic/orange.png"/>
+          <SProductPageSmallImage src="assets/images/computerTechnic/orange.png"/>
+          <SProductPageSmallImage src="assets/images/computerTechnic/orange.png"/>
+          <SProductPageSmallImage src="assets/images/computerTechnic/orange.png"/>
+        </SProductPageSmallImages>
+        <SProductPageMainImage>
+          <SProductPageBigImage src="assets/images/computerTechnic/orange.png"/>
+        </SProductPageMainImage>
+      </SProductPageImages>
+      <SProductPageOptions>
+        sdds
+      </SProductPageOptions>
+    </SProductPage>
   )
 }
