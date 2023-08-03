45cb9f85f3e934afada430f3fcbfb43b65e268b7
vazha2000 <vaja.siqtura2000@gmail.com>
2023-08-03 12:42:01 +0400

    add input container

--- src/views/ProductPage/ProductPage.jsx
+++ src/views/ProductPage/ProductPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {
   SProductCurrentQuantity,
+  SProductCurrentQuantityContainer,
   SProductPage,
   SProductPageBigImage,
   SProductPageImages,
@@ -39,13 +40,13 @@ export const ProductPage = ({ product }) => {
         <SProductQuantityBuyFavorites>
           <SProductQuantity>
             <SProductQuantityMinus>
-              <img src="assets/svg/minus.svg" alt="" />
+              <img src="assets/svg/minus.svg" alt="minus" />
             </SProductQuantityMinus>
-            <SProductCurrentQuantity>
-              1
-            </SProductCurrentQuantity>
+            <SProductCurrentQuantityContainer>
+              <SProductCurrentQuantity />
+            </SProductCurrentQuantityContainer>
             <SProductQuantityPlus>
-            <svg width="64px" height="64px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <rect width="24" height="24" fill="white"></rect> <path d="M12 6V18" stroke="#000000" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M6 12H18" stroke="#000000" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+              <img src="assets/svg/plus.svg" alt="plus" />
             </SProductQuantityPlus>
           </SProductQuantity>
         </SProductQuantityBuyFavorites>
