a3de083ba23d1d4c2caa1a898702ebe1ca817336
vazha2000 <vaja.siqtura2000@gmail.com>
2023-08-03 13:42:07 +0400

    add header to the table

--- src/components/ProductDescriptions/ProductDescriptions.jsx
+++ src/components/ProductDescriptions/ProductDescriptions.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import { SProductDescription, SProductDescriptionTable, SProductDescriptionTableRow } from "./ProductDescriptions.styled";
+import { SProductDescription, SProductDescriptionTable, SProductDescriptionTableHeader, SProductDescriptionTableRow } from "./ProductDescriptions.styled";
 
 export const ProductDescriptions = () => {
   return (
     <SProductDescription>
       <SProductDescriptionTable>
         <tbody>
+          <SProductDescriptionTableHeader>
+            <td>დეტალური მახასიათებლები</td>
+          </SProductDescriptionTableHeader>
           <SProductDescriptionTableRow>
             <td>koko</td>
             <td>koko</td>
