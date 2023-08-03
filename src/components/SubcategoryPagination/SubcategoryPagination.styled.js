ad19ccf3a5854c21864d9efa0696f55c54ccd546
vazha2000 <vaja.siqtura2000@gmail.com>
2023-08-03 19:39:36 +0400

    add style to page number items

---
+++ src/components/SubcategoryPagination/SubcategoryPagination.styled.js
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+export const SSubcategoryPagination = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 2rem 0;
+`;
+
+export const SSubcategoryPaginationList = styled.ul`
+  gap: .6rem;
+`;
+
+export const SSubcategoryPaginationListItems = styled.li`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 40px;
+  height: 40px;
+  border-radius: 4px;
+  background-color: ${({isActive}) => (isActive ? "#e81318" : "#babfbb")};
+  cursor: pointer;
+  font-weight: ${({isActive}) => (isActive ? "bold" : "normal")};
+  color: ${({isActive}) => (isActive ? "#fff" : "#1f1f1f")};
+
+  &:hover {
+    filter: ${({isActive}) => (!isActive && "brightness(80%)")};
+  }
+`;
+
+export const SCubcategoryPaginationButton = styled.button`
+  background-color: #df3d41;
+  width: 40px;
+  height: 40px;
+  border-radius: 4px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  rotate: ${({rotate}) => (rotate ? "180deg" : "0")};
+`
