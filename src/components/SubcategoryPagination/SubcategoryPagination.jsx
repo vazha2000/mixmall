e4d58d1401ab0b02ffec81a525043140fc421654
vazha2000 <vaja.siqtura2000@gmail.com>
2023-08-03 19:39:05 +0400

    add pagination functionality; add ellipsis components

--- src/components/SubcategoryPagination/SubcategoryPagination.jsx
+++ src/components/SubcategoryPagination/SubcategoryPagination.jsx
@@ -1,25 +1,66 @@
-import React from 'react'
-import { SSubcategoryPagination } from './SubcategoryPagination.styled'
+import React from "react";
+import {
+  SCubcategoryPaginationButton,
+  SSubcategoryPagination,
+  SSubcategoryPaginationList,
+  SSubcategoryPaginationListItems,
+} from "./SubcategoryPagination.styled";
 
-export const SubcategoryPagination = ({item, onPageChange, currentPage, itemsPerPage}) => {
-  console.log(item)
-  const totalPages = Math.ceil(item.products.length / itemsPerPage)
+export const SubcategoryPagination = ({
+  item,
+  onPageChange,
+  currentPage,
+  itemsPerPage,
+}) => {
+  console.log(item);
+  const totalPages = Math.ceil(item.products.length / itemsPerPage);
+  const maxPageNumbersToShow = 5;
+  const pageNumbers = Array.from(
+    { length: totalPages },
+    (_, index) => index + 1
+  );
+  const getPageNumbersWithEllipsis = () => {
+    if (totalPages <= 3) {
+      return pageNumbers;
+    }
 
-  const pageNumbers = Array.from({length: totalPages}, (_, index) => index + 1)
-  console.log(pageNumbers)
+    const leftEllipsis = currentPage - Math.floor(maxPageNumbersToShow / 2);
+    const rightEllipsis = currentPage + Math.floor(maxPageNumbersToShow / 2);
+
+    if (leftEllipsis <= 1) {
+      return [...pageNumbers.slice(0, maxPageNumbersToShow - 1), "ellipsis", totalPages];
+    } else if (rightEllipsis >= totalPages) {
+      return [1, "ellipsis", ...pageNumbers.slice(totalPages - maxPageNumbersToShow + 2)];
+    } else {
+      return [1, "ellipsis", ...pageNumbers.slice(leftEllipsis, rightEllipsis), "ellipsis", totalPages];
+    }
+  };
+
+  
   return (
     <SSubcategoryPagination>
-      <ul style={{display: "flex"}}>
-        {pageNumbers.map((page) => (
-          <li 
-          key={page}
-           onClick={() => onPageChange(page)}
-          style={{ cursor: "pointer", fontWeight: currentPage === page ? "bold" : "normal" }}
-          >
-            {page}
-          </li>
+      <SSubcategoryPaginationList style={{ display: "flex" }}>
+        <SCubcategoryPaginationButton>
+          <img src="assets/svg/paginationLeft.svg" alt="left" />
+        </SCubcategoryPaginationButton>
+        {getPageNumbersWithEllipsis().map((page, index) => (
+          <React.Fragment key={index}>
+            {page === "ellipsis" ? (
+              <span>...</span>
+            ) : (
+              <SSubcategoryPaginationListItems
+                onClick={() => onPageChange(page)}
+                isActive={currentPage === page}
+              >
+                {page}
+              </SSubcategoryPaginationListItems>
+            )}
+          </React.Fragment>
         ))}
-      </ul>
+        <SCubcategoryPaginationButton rotate={true}>
+          <img src="assets/svg/paginationLeft.svg" alt="left" />
+        </SCubcategoryPaginationButton>
+      </SSubcategoryPaginationList>
     </SSubcategoryPagination>
-  )
-}
+  );
+};
