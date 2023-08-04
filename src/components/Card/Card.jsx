5ed0e9cdc3b387a5afe753cee116e6e4354d218f
vazha2000 <vaja.siqtura2000@gmail.com>
2023-08-04 12:59:54 +0400

    add path to image; add path to product name

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import {
   SAddToCardButton,
   SCard,
@@ -13,6 +13,8 @@ import {
   SCardWishlist,
 } from "./Card.styled";
 import { AnimatePresence } from "framer-motion";
+import { WishlistContext } from "../../context/WishlistContext";
+import { SStyledLink } from "../DropdownMenu/DropdownMenu.styled";
 
 export const Card = (props) => {
   const {
@@ -25,9 +27,22 @@ export const Card = (props) => {
     alt,
     discountRate,
     isDiscount,
-    width
+    width,
+    index,
+    path
   } = props;
+  
+  const {wishlist, addToWishlist} = useContext(WishlistContext)
 
+  const isProductInWishlist = wishlist.includes(productName);
+
+  const handleWishlistClick = () => {
+    if (isProductInWishlist) {
+      console.log("Product is already in the wishlist!");
+    } else {
+      addToWishlist(productName);
+    }
+  };
   return (
     <SCard
       onMouseEnter={handleHover}
@@ -36,8 +51,10 @@ export const Card = (props) => {
       width={width}
     >
       <SCardImageContainer>
-        <SCardImage src={productImage} alt={alt} />
-        <SCardWishlist src="../assets/svg/wishlist.svg" alt="favorites icon" />
+        <SStyledLink to={path}>
+          <SCardImage src={productImage} alt={alt} />
+        </SStyledLink>
+        <SCardWishlist src="../assets/svg/wishlist.svg" alt="favorites icon" onClick={handleWishlistClick}/>
         {isDiscount && <SCardSaleDiscount>{discountRate}</SCardSaleDiscount>}
         <AnimatePresence>
           <SAddToCardButton initial={{y: "100%", opacity: 1}} animate={isHovered ? {y: 0, opacity: 1, transition: {stiffness: 0, duration: 0.2}} : {y: "100%", opacity: 1, transition: {stiffness: 0, duration: 0.2}}}>
@@ -46,7 +63,9 @@ export const Card = (props) => {
         </AnimatePresence>
       </SCardImageContainer>
       <SCardInfo>
-        <SCardProductName>{productName}</SCardProductName>
+        <SStyledLink to={path}>
+          <SCardProductName>{productName}</SCardProductName>
+        </SStyledLink>
         <SCardPriceContainer>
           <SCardPrice>{currentPrice}</SCardPrice>
           {isDiscount && <SCardOldPrice>{oldPrice}</SCardOldPrice>}
