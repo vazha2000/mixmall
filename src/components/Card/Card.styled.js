5935faf87fd74cc1c06d46feae80aa9ff077eb20
vazha2000 <vaja.siqtura2000@gmail.com>
2023-08-04 20:14:38 +0400

    update style

--- src/components/Card/Card.styled.js
+++ src/components/Card/Card.styled.js
@@ -107,9 +107,8 @@ position: fixed;
 top: 0;
 left: 50%;
 transform: translateX(-50%);
-width: 50%;
+width: 90%;
 z-index: 5;
-
 @media screen and (max-width: 600px){
   width: 80%;
 }
@@ -118,6 +117,7 @@ z-index: 5;
 export const SProductAddedToWishlist = styled(motion.div)`
   width: 100%;
   height: 80px;
+  white-space: nowrap;
   background-color: #df3d41;
   display: flex;
   align-items: center;
@@ -125,4 +125,8 @@ export const SProductAddedToWishlist = styled(motion.div)`
   border-radius: 5px;
   color: #fff;
   text-align: center;
+  
+  @media screen and (max-width: 600px){
+  white-space: normal;
+}
 `
