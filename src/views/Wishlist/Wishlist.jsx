80bc8760d42a1ba24a100e613a29b896e610db7a
vazha2000 <vaja.siqtura2000@gmail.com>
2023-08-05 12:13:29 +0400

    remove console log

--- src/views/Wishlist/Wishlist.jsx
+++ src/views/Wishlist/Wishlist.jsx
@@ -6,7 +6,6 @@ import { useNavigate } from "react-router-dom";
 
 export const Wishlist = () => {
   const { wishlist } = useContext(WishlistContext);
-  console.log(wishlist)
 
   const navigate = useNavigate();
 
