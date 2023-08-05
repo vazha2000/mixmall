5ac96835d9626f9cdc45b78cf649f8144d5151a8
vazha2000 <vaja.siqtura2000@gmail.com>
2023-08-05 11:56:16 +0400

    add handleProductClick function; pass down handleProductClick to Card as prop

--- src/views/Wishlist/Wishlist.jsx
+++ src/views/Wishlist/Wishlist.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { WishlistContext } from "../../context/WishlistContext";
+import { SWishlist, SWishlistEmpty } from "./Wishlist.styled";
+import { Card } from "../../components/Card/Card";
+import { useNavigate } from "react-router-dom";
 
 export const Wishlist = () => {
+  const { wishlist } = useContext(WishlistContext);
+
+  const navigate = useNavigate();
+
+  const handleProductClick = (product) => {
+    const productPath = product.subcategoryPath + product.path;
+    navigate(productPath);
+  };
+
   return (
-    <div>Wishlist</div>
-  )
-}
+    <SWishlist quantity={wishlist.length > 2}>
+      {wishlist.length === 0 && (
+        <SWishlistEmpty>შენი სურვილების სია ცარიელია</SWishlistEmpty>
+      )}
+      {wishlist.map((item, index) => (
+        <Card
+          key={index}
+          productImage={item.productImage}
+          productName={item.productName}
+          currentPrice={item.currentPrice}
+          oldPrice={item.oldPrice}
+          discountRate={item.discountRate}
+          isDiscount={item.isDiscount}
+          onClick={() => handleProductClick(item)}
+          />
+      ))}
+    </SWishlist>
+  );
+};
