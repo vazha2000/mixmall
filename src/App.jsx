13c7911b74fb0292bad1d09d9b0143c17327a4d2
vazha2000 <vaja.siqtura2000@gmail.com>
2023-08-06 15:25:00 +0400

    add CheckoutProvider

--- src/App.jsx
+++ src/App.jsx
@@ -7,38 +7,41 @@ import { categoriesListItems } from "./data/data";
 import { ProductPage } from "./views/ProductPage/ProductPage";
 import { WishlistProvider } from "./context/WishlistContext";
 import { Wishlist } from "./views/Wishlist";
+import { CheckoutProvider } from "./context/CheckoutContext";
 
 function App() {
   return (
     <WishlistProvider>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          {categoriesListItems.map((category, index) => {
-            return (
-              <Route key={index} path={category.categoryName}>
-                {category.subcategories.map((subcategory, index) => (
-                  <Route
-                    key={index}
-                    path={`${subcategory.subcategoryName}`}
-                    element={<Subcategories item={subcategory} />}
-                  />
-                ))}
-                {category.subcategories.map((subcategory, index) =>
-                  subcategory.products.map((product) => (
+      <CheckoutProvider>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+            {categoriesListItems.map((category, index) => {
+              return (
+                <Route key={index} path={category.categoryName}>
+                  {category.subcategories.map((subcategory, index) => (
                     <Route
                       key={index}
-                      path={`${subcategory.subcategoryName}/${product.productName}`}
-                      element={<ProductPage product={product} />}
+                      path={`${subcategory.subcategoryName}`}
+                      element={<Subcategories item={subcategory} />}
                     />
-                  ))
-                )}
-              </Route>
-            );
-          })}
-          <Route path="/wishlist" element={<Wishlist />}/>
-        </Routes>
-      </Layout>
+                  ))}
+                  {category.subcategories.map((subcategory, index) =>
+                    subcategory.products.map((product) => (
+                      <Route
+                        key={index}
+                        path={`${subcategory.subcategoryName}/${product.productName}`}
+                        element={<ProductPage product={product} />}
+                      />
+                    ))
+                  )}
+                </Route>
+              );
+            })}
+            <Route path="/wishlist" element={<Wishlist />} />
+          </Routes>
+        </Layout>
+      </CheckoutProvider>
     </WishlistProvider>
   );
 }
