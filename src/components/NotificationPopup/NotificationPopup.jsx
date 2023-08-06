eeffe2dc237f93d74585b2805776fe3c04e1089c
vazha2000 <vaja.siqtura2000@gmail.com>
2023-08-06 16:50:17 +0400

    update code temporarily

--- src/components/NotificationPopup/NotificationPopup.jsx
+++ src/components/NotificationPopup/NotificationPopup.jsx
@@ -13,25 +13,59 @@ export const NotificationPopup = ({
   productName,
 }) => {
   return (
+    <>
     <AnimatePresence>
-      {(showCartPopup ||
-        showCartRemovePopup ||
-        showWishlistPopup ||
-        showWishlistRemovePopup) && (
+      {showCartPopup && (
         <SCenteredContainer>
           <SProductAddedToWishlist
             initial={{ y: 0 }}
             animate={{ y: 30 }}
             exit={{ y: -40, opacity: 0, transition: { duration: 0.3 } }}
           >
-            პროდუქტი {productName}{" "}
-            {(showCartPopup || showWishlistPopup) && "დაემატა"}{" "}
-            {(showWishlistRemovePopup || showCartRemovePopup) && "წაიშალა"}{" "}
-            {showCartPopup && "კალათაში"} {showCartRemovePopup && "კალათიდან"}
-            {showWishlistPopup && "სურვილების სიაში"} {showWishlistRemovePopup && "სურვილების სიიდან"}
+            პროდუქტი {productName} დაემატა კალათაში
           </SProductAddedToWishlist>
         </SCenteredContainer>
       )}
     </AnimatePresence>
+    <AnimatePresence>
+      {showCartRemovePopup && (
+        <SCenteredContainer>
+          <SProductAddedToWishlist
+            initial={{ y: 0 }}
+            animate={{ y: 30 }}
+            exit={{ y: -40, opacity: 0, transition: { duration: 0.3 } }}
+          >
+            პროდუქტი {productName} წაიშალა კალათიდან
+          </SProductAddedToWishlist>
+        </SCenteredContainer>
+      )}
+    </AnimatePresence>
+    <AnimatePresence>
+      {showWishlistPopup && (
+        <SCenteredContainer>
+          <SProductAddedToWishlist
+            initial={{ y: 0 }}
+            animate={{ y: 30 }}
+            exit={{ y: -40, opacity: 0, transition: { duration: 0.3 } }}
+          >
+            პროდუქტი {productName} დაემატა სურვილების სიაში
+          </SProductAddedToWishlist>
+        </SCenteredContainer>
+      )}
+    </AnimatePresence>
+    <AnimatePresence>
+      {showWishlistRemovePopup && (
+        <SCenteredContainer>
+          <SProductAddedToWishlist
+            initial={{ y: 0 }}
+            animate={{ y: 30 }}
+            exit={{ y: -40, opacity: 0, transition: { duration: 0.3 } }}
+          >
+            პროდუქტი {productName} წაიშალა სურვილების სიიდან
+          </SProductAddedToWishlist>
+        </SCenteredContainer>
+      )}
+    </AnimatePresence>
+    </>
   );
 };
