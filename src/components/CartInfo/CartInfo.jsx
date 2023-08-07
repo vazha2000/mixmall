7962fa7349ebdb12d1574ba628d962cf4cd4c40c
vazha2000 <vaja.siqtura2000@gmail.com>
2023-08-07 16:15:02 +0400

    add cartinfo table

--- src/components/CartInfo/CartInfo.jsx
+++ src/components/CartInfo/CartInfo.jsx
@@ -1,7 +1,36 @@
 import React from 'react'
+import { SCartCardTable, SCartCardTableBody, SCartCardTableBox, SCartCardTableBoxImageContent, SCartCardTableBoxNameContent, SCartCardTableBoxQuantityContent, SCartCardTableBoxTotalContent, SCartCardTablleRemove, SCartInfo } from './CartInfo.styled'
 
-export const CartInfo = () => {
+export const CartInfo = ({cart}) => {
   return (
-    <div>CartInfo</div>
+    <SCartInfo>
+              {cart.length === 0 ? (
+                <div>კალათა ცარიელია</div>
+              ) : (
+                <SCartCardTable>
+                  <SCartCardTableBody>
+                    {cart.map((item, index) => (
+                      <SCartCardTableBox key={index}>
+                        <SCartCardTableBoxImageContent>
+                          <img src={item.productImage} height={"100%"} alt="" />
+                        </SCartCardTableBoxImageContent>
+                        <SCartCardTableBoxNameContent>
+                          {item.productName}
+                        </SCartCardTableBoxNameContent>
+                        <SCartCardTableBoxQuantityContent>
+                          22 ცალი
+                        </SCartCardTableBoxQuantityContent>
+                        <SCartCardTableBoxTotalContent>
+                          {item.currentPrice}00 ლარი
+                        </SCartCardTableBoxTotalContent>
+                        <SCartCardTablleRemove>
+                          X
+                        </SCartCardTablleRemove>
+                      </SCartCardTableBox>
+                    ))}
+                  </SCartCardTableBody>
+                </SCartCardTable>
+              )}
+            </SCartInfo>
   )
 }
