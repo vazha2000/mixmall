3ede566885f46c3d3497afb14da3587261070a1a
vazha2000 <vaja.siqtura2000@gmail.com>
2023-08-07 17:16:41 +0400

    update style

--- src/components/CartInfo/CartInfo.styled.js
+++ src/components/CartInfo/CartInfo.styled.js
@@ -6,7 +6,6 @@ export const SCartInfo = styled(motion.div)`
   top: 40px;
   right: 0;
   background-color: #fff;
-  padding: 8px 0;
   z-index: 5;
   padding: 8px 10px;
 `
@@ -37,7 +36,6 @@ export const SCartCardTableBox = styled.tr`
   height: 70px;
   display: flex;
   align-items: center;
-  padding: 0 5px;
   border: 1px solid black;
   font-size: 15px;
 `;
@@ -46,13 +44,20 @@ export const SCartCardTableBoxContent = styled(BaseTableCell)``;
 export const SCartCardTableBoxImageContent = styled(BaseTableCell)``;
 export const SCartCardTableBoxNameContent = styled(BaseTableCell)`
   min-width: 200px;
-  padding-left: 5px;
 `;
 export const SCartCardTableBoxQuantityContent = styled(BaseTableCell)``;
 export const SCartCardTableBoxTotalContent = styled(BaseTableCell)`
   min-width: 140px;
 `;
 
-export const SCartCardTablleRemove = styled.td`
-  padding: 0 10px;
+export const SCartCardTablleRemove = styled(BaseTableCell)`
+  border: none;
+  padding: 0;
+  justify-content: center;
+  width: 50px;
+
+  > img {
+    width: 20px;
+    cursor: pointer;
+  }
 `
