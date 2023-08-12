dc37b8596863c1a905cf486c0f6df73245494d17
vazha2000 <vaja.siqtura2000@gmail.com>
2023-08-12 20:30:10 +0400

    add svg icon

--- src/components/DropdownMenu/DropdownMenu.jsx
+++ src/components/DropdownMenu/DropdownMenu.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
-import { SDropDownMenu, SMenuGridContainer, SMenuItems, SStyledLink } from './DropdownMenu.styled';
-import { useState } from 'react';
-import { useRef } from 'react';
-import { useEffect } from 'react';
+import React from "react";
+import {
+  SDropDownMenu,
+  SMenuGridContainer,
+  SMenuItems,
+  SMenuItemsDot,
+  SStyledLink,
+} from "./DropdownMenu.styled";
+import { useState } from "react";
+import { useRef } from "react";
+import { useEffect } from "react";
 
-export const DropdownMenu = ({item, topPosition}) => {
-  const menuRef = useRef(null)
+export const DropdownMenu = ({ item }) => {
+  const menuRef = useRef(null);
   const [isOverflowing, setIsOverflowing] = useState(false);
 
   useEffect(() => {
     function handleResize() {
-
       if (menuRef.current) {
         const { right } = menuRef.current.getBoundingClientRect();
         const windowWidth = window.innerWidth;
@@ -18,30 +23,43 @@ export const DropdownMenu = ({item, topPosition}) => {
       }
     }
 
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
     handleResize();
 
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
-  
+
   return (
     <SDropDownMenu
-      initial={{ clipPath: 'inset(0% 100% 0% 0%)', opacity: 1 }}
-      animate={{ clipPath: 'inset(0% 0% 0% 0%)', opacity: 1, transition: {duration: 0.3, delay: 0.2} }} 
-      exit={{ clipPath: 'inset(0% 100% 0% 0%)', opacity: 1, transition: {delay: 0.1, duration: 0.3}}} 
-      transition={{type: "tween", duration: 0.4, delay: 0.1}}
-      topPosition={topPosition}
+      initial={{ clipPath: "inset(0% 100% 0% 0%)", opacity: 1 }}
+      animate={{
+        clipPath: "inset(0% 0% 0% 0%)",
+        opacity: 1,
+        transition: { duration: 0.3, delay: 0.2 },
+      }}
+      exit={{
+        clipPath: "inset(0% 100% 0% 0%)",
+        opacity: 1,
+        transition: { delay: 0.1, duration: 0.3 },
+      }}
+      transition={{ type: "tween", duration: 0.4, delay: 0.1 }}
       ref={menuRef}
     >
       <SMenuGridContainer isOverflowing={isOverflowing}>
         {item.subcategories.map((subcategory, index) => (
-          <SStyledLink to={`/${item.categoryName}/${subcategory.subcategoryName}`}  key={index}>
-            <SMenuItems>{subcategory.name}</SMenuItems>
+          <SStyledLink
+            to={`/${item.categoryName}/${subcategory.subcategoryName}`}
+            key={index}
+          >
+            <SMenuItemsDot>
+              <img src="assets/svg/dot.svg" />
+              <SMenuItems>{subcategory.name}</SMenuItems>
+            </SMenuItemsDot>
           </SStyledLink>
         ))}
       </SMenuGridContainer>
-    </SDropDownMenu>    
+    </SDropDownMenu>
   );
-}
+};
