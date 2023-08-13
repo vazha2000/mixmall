63e73ec2f265e45fa9f8de156122903ea6f828a3
vazha2000 <vaja.siqtura2000@gmail.com>
2023-08-13 14:13:55 +0400

    add overlay functionality; add overlay style

--- src/components/CartInfo/CartInfoOverlay/CartInfoOverlay.jsx
+++ src/components/CartInfo/CartInfoOverlay/CartInfoOverlay.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import { SCartInfoOverlay } from './CartInfoOverlay.styled'
 
-export const CartInfoOverlay = () => {
+export const CartInfoOverlay = ({isCartClicked, handleCartClick}) => {
+  const overlayVariants = {
+    hidden: { opacity: 0},
+    visible: {opacity: 1}
+  }
   return (
-    <div>CartInfoOverlay</div>
+    <SCartInfoOverlay
+    onClick={handleCartClick}
+    initial="hidden"
+    animate={isCartClicked ? "visible" : "hidden"}
+    exit="hidden"
+    variants={overlayVariants}
+    />
   )
 }
