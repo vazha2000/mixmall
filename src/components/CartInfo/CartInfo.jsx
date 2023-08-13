3ffd874f00f0f49de9435f7e9b580b0c67613056
vazha2000 <vaja.siqtura2000@gmail.com>
2023-08-13 14:15:14 +0400

    update cartInfo functionality; add overlay functionality; update style

--- src/components/CartInfo/CartInfo.jsx
+++ src/components/CartInfo/CartInfo.jsx
@@ -5,73 +5,162 @@ import {
   SCartCardTableBox,
   SCartCardTableBoxImageContent,
   SCartCardTableBoxNameContent,
-  SCartCardTableBoxQuantityContent,
-  SCartCardTableBoxTotalContent,
   SCartCardTablleRemove,
   SCartEmpty,
   SCartInfo,
+  SCartInfoHeaderClose,
+  SCartInfoHeaderContainer,
+  SCartInfoHeaderIconContent,
 } from "./CartInfo.styled";
 import { SStyledLink } from "../DropdownMenu/DropdownMenu.styled";
+import { CartInfoOverlay } from "./CartInfoOverlay/CartInfoOverlay";
 
-export const CartInfo = ({ cart, removeFromCart }) => {
+export const CartInfo = ({
+  cart,
+  removeFromCart,
+  setIsCartClicked,
+  isCartClicked,
+}) => {
+  console.log(cart);
   const handleRemoveClick = (item) => {
     removeFromCart(item);
   };
+  const handleCartClick = () => {
+    setIsCartClicked(!isCartClicked);
+  };
   return (
+    <>
     <SCartInfo
-      initial={{ clipPath: "inset(0% 0% 100% 0%)", opacity: 1 }}
-      animate={{
-        clipPath: "inset(0% 0% 0% 0%)",
-        opacity: 1,
-        transition: { duration: 0.3, delay: 0.2 },
-      }}
-      exit={{
-        clipPath: "inset(0% 0% 100% 0%)",
-        opacity: 1,
-        transition: { delay: 0.1, duration: 0.3 },
-      }}
+      initial={{ x: "100%" }}
+      animate={{ x: 0 }}
+      exit={{ x: "100%" }}
       transition={{ type: "tween", duration: 0.4, delay: 0.1 }}
     >
-      {cart.length === 0 ? (
-        <SCartEmpty>კალათა ცარიელია</SCartEmpty>
-      ) : (
-        <>
-          <SCartCardTable>
-            <SCartCardTableBody>
-              {cart.map((item, index) => (
-                <SCartCardTableBox key={index}>
-                  <SCartCardTableBoxImageContent>
-                    <img
-                      src={item.productImage}
-                      height={"100%"}
-                      alt={item.alt}
-                    />
-                  </SCartCardTableBoxImageContent>
-                  <SCartCardTableBoxNameContent>
-                    {item.productName}
-                  </SCartCardTableBoxNameContent>
-                  <SCartCardTableBoxQuantityContent>
-                    {item.productQuantity} ცალი
-                  </SCartCardTableBoxQuantityContent>
-                  <SCartCardTableBoxTotalContent>
-                    {item.currentPrice * item.productQuantity}
-                  </SCartCardTableBoxTotalContent>
-                  <SCartCardTablleRemove
-                    onClick={() => handleRemoveClick(item)}
-                  >
-                    <img src="assets/svg/trash.svg" alt="remove" />
-                  </SCartCardTablleRemove>
-                </SCartCardTableBox>
-              ))}
-            </SCartCardTableBody>
-          </SCartCardTable>
-          <div style={{ padding: "10px 0 0", textAlign: "end" }}>
-            <SStyledLink to="/checkout">
-              <strong style={{ cursor: "pointer" }}>გაგრძელება</strong>
-            </SStyledLink>
-          </div>
-        </>
-      )}
+      <SCartInfoHeaderContainer>
+        <SCartInfoHeaderIconContent>
+          <img src="assets/svg/cart.svg" alt="cart icon" />
+          <h3>კალათა</h3>
+        </SCartInfoHeaderIconContent>
+        <SCartInfoHeaderClose>
+          <img
+            src="assets/svg/close.svg"
+            alt="close icon"
+            onClick={() => setIsCartClicked(false)}
+          />
+        </SCartInfoHeaderClose>
+      </SCartInfoHeaderContainer>
+      <SCartCardTable>
+        <SCartCardTableBody>
+          {cart.map((item, index) => (
+            <SCartCardTableBox key={index}>
+              <SCartCardTableBoxImageContent>
+                <img src={item.productImage} height={"100%"} alt={item.alt} />
+              </SCartCardTableBoxImageContent>
+              <SCartCardTableBoxNameContent>
+                <div>{item.productName}</div>
+                <div>
+                  {item.productQuantity} x{" "}
+                  <span>
+                    {item.currentPrice}
+                    <img src="assets/svg/lari.svg" alt="lari icon" />
+                  </span>
+                </div>
+              </SCartCardTableBoxNameContent>
+              <SCartCardTablleRemove onClick={() => handleRemoveClick(item)}>
+                <img src="assets/svg/trash.svg" alt="remove" />
+              </SCartCardTablleRemove>
+            </SCartCardTableBox>
+          ))}
+        </SCartCardTableBody>
+      </SCartCardTable>
+      
     </SCartInfo>
+    {isCartClicked && (
+      <>
+        <CartInfoOverlay
+          isCartClicked={isCartClicked}
+          handleCartClick={handleCartClick}
+        />
+      </>
+    )}
+  </>
   );
 };
+
+// import React from "react";
+// import {
+//   SCartCardTable,
+//   SCartCardTableBody,
+//   SCartCardTableBox,
+//   SCartCardTableBoxImageContent,
+//   SCartCardTableBoxNameContent,
+//   SCartCardTableBoxQuantityContent,
+//   SCartCardTableBoxTotalContent,
+//   SCartCardTablleRemove,
+//   SCartEmpty,
+//   SCartInfo,
+// } from "./CartInfo.styled";
+// import { SStyledLink } from "../DropdownMenu/DropdownMenu.styled";
+
+// export const CartInfo = ({ cart, removeFromCart }) => {
+//   const handleRemoveClick = (item) => {
+//     removeFromCart(item);
+//   };
+//   return (
+//     <SCartInfo
+//       initial={{ clipPath: "inset(0% 0% 100% 0%)", opacity: 1 }}
+//       animate={{
+//         clipPath: "inset(0% 0% 0% 0%)",
+//         opacity: 1,
+//         transition: { duration: 0.3, delay: 0.2 },
+//       }}
+//       exit={{
+//         clipPath: "inset(0% 0% 100% 0%)",
+//         opacity: 1,
+//         transition: { delay: 0.1, duration: 0.3 },
+//       }}
+//       transition={{ type: "tween", duration: 0.4, delay: 0.1 }}
+//     >
+//       {cart.length === 0 ? (
+//         <SCartEmpty>კალათა ცარიელია</SCartEmpty>
+//       ) : (
+//         <>
+// <SCartCardTable>
+//   <SCartCardTableBody>
+//     {cart.map((item, index) => (
+//       <SCartCardTableBox key={index}>
+//         <SCartCardTableBoxImageContent>
+//           <img
+//             src={item.productImage}
+//             height={"100%"}
+//             alt={item.alt}
+//           />
+//         </SCartCardTableBoxImageContent>
+//         <SCartCardTableBoxNameContent>
+//           {item.productName}
+//         </SCartCardTableBoxNameContent>
+//         <SCartCardTableBoxQuantityContent>
+//           {item.productQuantity} ცალი
+//         </SCartCardTableBoxQuantityContent>
+//         <SCartCardTableBoxTotalContent>
+//           {item.currentPrice * item.productQuantity}
+//         </SCartCardTableBoxTotalContent>
+//         <SCartCardTablleRemove
+//           onClick={() => handleRemoveClick(item)}
+//         >
+//           <img src="assets/svg/trash.svg" alt="remove" />
+//         </SCartCardTablleRemove>
+//       </SCartCardTableBox>
+//     ))}
+//   </SCartCardTableBody>
+// </SCartCardTable>
+//           <div style={{ padding: "10px 0 0", textAlign: "end" }}>
+//             <SStyledLink to="/checkout">
+//               <strong style={{ cursor: "pointer" }}>გაგრძელება</strong>
+//             </SStyledLink>
+//           </div>
+//         </>
+//       )}
+//     </SCartInfo>
+//   );
+// };
--- src/components/CartInfo/CartInfo.styled.js
+++ src/components/CartInfo/CartInfo.styled.js
@@ -3,33 +3,80 @@ import styled from "styled-components";
 
 export const SCartInfo = styled(motion.div)`
   position: absolute;
-  top: 40px;
+  top: 0;
   right: 0;
-  background-color: #fff;
+  background-color: #f8f8f8;
   z-index: 5;
-  border: 1px solid rgba(0,0,0, 0.3);
-  padding: 8px 10px;
-`
+  border: 1px solid rgba(0, 0, 0, 0.3);
+  width: 675px;
+  height: 100%;
+
+  @media screen and (max-width: 1150px) {
+    width: 100%;
+  }
+`;
+
+export const SCartInfoHeaderContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 25px;
+  height: 120px;
+  width: 100%;
+  background: linear-gradient(45deg,#df3d41, #e67a7c);
+  color: #fff;
+  letter-spacing: 2px;
+`;
+
+export const SCartInfoHeaderIconContent = styled.div`
+  display: flex;
+  align-items: flex-end;
+  gap: 10px;
+
+  > h3 {
+    pointer-events: none;
+  }
+
+  > img {
+    width: 30px;
+    filter: brightness(0) invert(1);
+  }
+`;
+
+export const SCartInfoHeaderClose = styled.div`
+  > img {
+    width: 20px;
+    filter: brightness(0) invert(1);
+    cursor: pointer;
+
+    &:hover {
+      filter: invert(40%) sepia(100%) saturate(3500%) hue-rotate(349deg)
+        brightness(0%) contrast(100%);
+    }
+  }
+`;
 
 export const SCartEmpty = styled.div`
   white-space: nowrap;
-`
+`;
 
 export const SCartCardTable = styled.table`
-  height: auto;
+  height: 125px;
   border-spacing: 0;
-`
+  width: 100%;
+  margin-top: 20px;
+`;
 
 export const SCartCardTableBody = styled.tbody`
   display: flex;
   flex-direction: column;
   height: inherit;
   gap: 10px;
-`
-
+  padding: 0 20px;
+`;
 
 const BaseTableCell = styled.td`
-  border-right: 1px solid black;
+  /* border-right: 1px solid black; */
   height: 100%;
   display: flex;
   align-items: center;
@@ -38,24 +85,46 @@ const BaseTableCell = styled.td`
 `;
 
 export const SCartCardTableBox = styled.tr`
-  height: 70px;
+  height: inherit;
   display: flex;
   align-items: center;
-  border: 1px solid black;
   font-size: 15px;
+  background-color: #fff;
+  padding: 5px;
+  border-radius: 10px;
 `;
 
 export const SCartCardTableBoxContent = styled(BaseTableCell)``;
-export const SCartCardTableBoxImageContent = styled(BaseTableCell)``;
+export const SCartCardTableBoxImageContent = styled(BaseTableCell)`
+  height: 90%;
+  border: 1px solid rgba(0, 0, 0, 0.3);
+  border-radius: inherit;
+  margin-left: 5px;
+`;
 export const SCartCardTableBoxNameContent = styled(BaseTableCell)`
   min-width: 200px;
+  align-items: flex-start;
+  padding: 8px 15px;
+  font-weight: bold;
+  color: #df3d41;
+  flex: 1;
+  flex-direction: column;
+  gap: 5px;
+
+  > div:nth-child(2) {
+    color: #1f1f1f;
+    font-weight: normal;
+    > span {
+      font-weight: bold;
+    }
+  }
 `;
-export const SCartCardTableBoxQuantityContent = styled(BaseTableCell)`
-  min-width: 90px;
-`;
-export const SCartCardTableBoxTotalContent = styled(BaseTableCell)`
-  min-width: 140px;
-`;
+// export const SCartCardTableBoxQuantityContent = styled(BaseTableCell)`
+//   min-width: 90px;
+// `;
+// export const SCartCardTableBoxTotalContent = styled(BaseTableCell)`
+//   min-width: 140px;
+// `;
 
 export const SCartCardTablleRemove = styled(BaseTableCell)`
   border: none;
@@ -67,4 +136,4 @@ export const SCartCardTablleRemove = styled(BaseTableCell)`
     width: 20px;
     cursor: pointer;
   }
-`
+`;
