9854a81ed1e346308bf252dad3d4d3064ca61311
vazha2000 <vaja.siqtura2000@gmail.com>
2023-08-15 19:21:33 +0400

    add image containers using grid layout

--- src/components/GridSubcategories/GridSubcategories.jsx
+++ src/components/GridSubcategories/GridSubcategories.jsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import React from "react";
+import {
+  SBigImageContainer,
+  SBigImgage,
+  SGridSubcategories,
+  SSmallImage,
+  SSmallImageWrapper,
+  SSmallImagesContainer,
+} from "./GridSubcategories.styled";
 
 export const GridSubcategories = () => {
   return (
-    <div>GridSubcategories</div>
-  )
-}
+    <>
+      <SGridSubcategories>
+        <SBigImageContainer>
+          <SBigImgage src="assets/images/watch.jpg" />
+        </SBigImageContainer>
+        <SSmallImagesContainer>
+          <SSmallImageWrapper>
+            <SSmallImage src="assets/images/watch.jpg" />
+          </SSmallImageWrapper>
+          <SSmallImageWrapper>
+            <SSmallImage src="assets/images/watch.jpg" />
+          </SSmallImageWrapper>
+          <SSmallImageWrapper>
+            <SSmallImage src="assets/images/watch.jpg" />
+          </SSmallImageWrapper>
+          <SSmallImageWrapper>
+            <SSmallImage src="assets/images/watch.jpg" />
+          </SSmallImageWrapper>
+          <SSmallImageWrapper>
+            <SSmallImage src="assets/images/watch.jpg" />
+          </SSmallImageWrapper>
+          <SSmallImageWrapper>
+            <SSmallImage src="assets/images/watch.jpg" />
+          </SSmallImageWrapper>
+        </SSmallImagesContainer>
+      </SGridSubcategories>
+    </>
+  );
+};
