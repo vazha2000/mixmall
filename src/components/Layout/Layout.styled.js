a46e90813588ca80f5fd1470aa0b97fb3d196811
vazha2000 <vaja.siqtura2000@gmail.com>
2023-08-17 14:59:43 +0400

    remove padding

--- src/components/Layout/Layout.styled.js
+++ src/components/Layout/Layout.styled.js
@@ -9,11 +9,11 @@ export const SLayout = styled.div`
   justify-content: space-between;
   box-sizing: border-box;
   background-color: #fff;
-  padding: 0 18px;
+  /* padding: 0 18px; */
 
   box-shadow: 5px 0px 10px rgba(0, 0, 0, 0.15), -5px 0px 10px rgba(0, 0, 0, 0.15);
 
   @media screen and (max-width: 1024px){
-    width: 97%;
+    width: 100%;
   }
 `
