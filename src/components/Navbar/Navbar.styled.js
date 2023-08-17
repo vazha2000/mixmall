651ca70709c821350500d9f89ca3d5ccd7203b7e
vazha2000 <vaja.siqtura2000@gmail.com>
2023-08-17 13:54:56 +0400

    update style

--- src/components/Navbar/Navbar.styled.js
+++ src/components/Navbar/Navbar.styled.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 
 export const SNavbar = styled.nav`
   width: 100%;
-  border-bottom: 1px solid rgba(0, 0, 0, 0.18);
+  /* border-bottom: 1px solid rgba(0, 0, 0, 0.18); */
   height: 6rem;
   display: flex;
   align-items: center;
@@ -14,47 +14,11 @@ export const SLogo = styled.img`
   cursor: pointer;
 `;
 
-export const SNavMenu = styled.ul`
-  display: flex;
-  gap: 2rem;
-  padding: 0 1rem;
-  @media screen and (max-width: 768px) {
-    display: none;
-  }
-`;
-
-export const SNavItem = styled.li`
-  position: relative;
-  cursor: pointer;
-  font-family: "BPG Mrgvlovani Caps 2010";
-  padding: 0.5rem;
-  border-radius: 5px;
-  text-align: center;
-  display: flex;
-  align-items: center;
-  font-size: 13px;
-  white-space: nowrap;
-  &:hover {
-    background-color: #df3d41;
-    color: #ffffff;
-  }
-  &:not(:last-child)::after {
-    content: "";
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    right: calc(-1.2rem + 1px);
-    width: 1px;
-    height: 50%;
-    transform: translateY(50%);
-    background-color: rgba(0, 0, 0, 0.2);
-    pointer-events: none;
-  }
-`;
-
 export const SNavActions = styled.div`
   display: flex;
-  width: 45%;
+  width: 90%;
+  gap: 1rem;
+  margin-left: 1rem;
   justify-content: space-between;
 
   @media screen and (max-width: 1024px) {
@@ -67,7 +31,7 @@ export const SNavActions = styled.div`
 
 export const SNavInputContainer = styled.div`
   display: flex;
-  width: 70%;
+  width: 100%;
   border-radius: 0.5rem;
 
   @media screen and (max-width: 1024px) {
@@ -75,10 +39,50 @@ export const SNavInputContainer = styled.div`
   }
 `;
 
+export const SNavContactContainer = styled.div`
+
+  > span {
+    font-weight: bold;
+  }
+
+  min-width: 240px;
+  gap: 20px;
+  background-color: #f0f0f0;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  padding: 0 4px;
+  border-radius: 5px;
+  white-space: nowrap;
+
+  &:hover {
+    background-color: #df3d41;
+    transition: 0.5s ease;
+    cursor: pointer;
+
+    > span {
+      color: #fff;
+    }
+  }
+`
+
+export const SNavContactIconContainer = styled.div`
+  background-color: red;
+  width: 38px;
+  height: 38px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: inherit;
+  background-color: #fff;
+  > img {
+    width: 30px;
+  }
+`
+
 export const SWishlistCartContainer = styled.div`
   display: flex;
   gap: 1rem;
-  margin-left: 1.5rem;
   position: relative;
 `;
 
