c6e84f3825cfb86036abdcf3bdb8cec8ae2889a7
vazha2000 <vaja.siqtura2000@gmail.com>
2023-08-18 15:17:44 +0400

    update footer design

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,31 +1,50 @@
-import React from 'react'
-import { SFooter, SFooterContact, SFooterInfo, SFooterLogo, SFooterLogoContainer, SFooterLogoSocial, SFooterSocial, SFooterSocialContainer } from './Footer.styled'
+import React from "react";
+import {
+  SFooter,
+  SFooterContact,
+  SFooterInfo,
+  SFooterInfoContact,
+  SFooterLogo,
+  SFooterLogoContainer,
+  SFooterLogoSocial,
+  SFooterSocial,
+  SFooterSocialContainer,
+} from "./Footer.styled";
 
 export const Footer = () => {
   return (
     <SFooter>
       <SFooterLogoSocial>
         <SFooterLogoContainer>
-          <SFooterLogo src='../assets/images/LogoDark.png' alt='BlackLogo'/>
+          <SFooterLogo src="../assets/images/LogoDark.png" alt="BlackLogo" />
         </SFooterLogoContainer>
         <SFooterSocialContainer>
-          <SFooterSocial src='../assets/svg/Footer/fb.svg' alt='facebook'/>
-          <SFooterSocial src='../assets/svg/Footer/instagram.svg' alt='instagram'/>
-          <SFooterSocial src='../assets/svg/Footer/tiktok.svg' alt='tiktok'/>
+          <SFooterSocial src="../assets/svg/Footer/fb.svg" alt="facebook" />
+          <SFooterSocial
+            src="../assets/svg/Footer/instagram.svg"
+            alt="instagram"
+          />
+          <SFooterSocial src="../assets/svg/Footer/tiktok.svg" alt="tiktok" />
         </SFooterSocialContainer>
       </SFooterLogoSocial>
-      <SFooterInfo>
-        <h4>საინფორმაციო</h4>
-        <span>ჩვენ შესახებ</span>
-        <span>საგარანტიო პირობები</span>
-        <span>მიწოდების პირობები</span>
-        <span>კონფიდენციალურობა</span>
-      </SFooterInfo>
-      <SFooterContact>
-        <h4>კონტაქტი</h4>
-        <span>579-33-33-11</span>
-        <span>mixmall.ge@gmail.com</span>
-      </SFooterContact>
+      <SFooterInfoContact>
+        <SFooterInfo>
+          <div>საინფორმაციო</div>
+          <div>
+            <span>ჩვენს შესახებ</span>
+            <span>საგარანტიო პირობები</span>
+            <span>მიწოდების პირობები</span>
+            <span>კონფიდენციალურობა</span>
+          </div>
+        </SFooterInfo>
+        <SFooterContact>
+          <div>კონტაქტი</div>
+          <div>
+            <span>579-33-33-11</span>
+            <span>mixmall.ge@gmail.com</span>
+          </div>
+        </SFooterContact>
+      </SFooterInfoContact>
     </SFooter>
-  )
-}
+  );
+};
--- src/components/Footer/Footer.styled.js
+++ src/components/Footer/Footer.styled.js
@@ -2,85 +2,98 @@ import styled from "styled-components";
 
 export const SFooter = styled.div`
   display: flex;
-  justify-content: space-between;
   width: 100%;
   background-color: #1f1f1f;
   color: #f2f2f2;
-  padding: 0 10%;
-
-  @media screen and (max-width: 1024px){
-    padding: 0 2%;
+  padding: 4% 5%;
+  gap: 80px;
+  @media screen and (max-width: 1024px) {
+    padding: 4% 5%;
   }
-`
+  @media screen and (max-width: 700px){
+    gap: 20px;
+    padding: 3% 1%;
+  }
+`;
 
 export const SFooterLogoSocial = styled.div`
   display: flex;
   flex-direction: column;
   gap: 30px;
-  padding: 7% 0;
-`
+`;
 
-export const SFooterLogoContainer = styled.div`
-
-`
+export const SFooterLogoContainer = styled.div``;
 
 export const SFooterLogo = styled.img`
   width: 120px;
 
-  @media screen and (max-width: 620px){
+  @media screen and (max-width: 620px) {
     width: 80px;
   }
-`
+`;
 
 export const SFooterSocialContainer = styled.div`
   display: flex;
   justify-content: space-between;
-`
+`;
 
 export const SFooterSocial = styled.img`
   width: 30px;
   cursor: pointer;
   filter: brightness(100%);
-  
+
   &:hover {
     filter: brightness(80%);
   }
-  @media screen and (max-width: 620px){
+  @media screen and (max-width: 620px) {
     width: 20px;
   }
-`
+`;
 
 export const SFooterInfo = styled(SFooterLogoSocial)`
+  > div {
+    display: flex;
+    gap: 30px;
+
+
+    @media screen and (max-width: 550px){
+        gap: 12px;
+        font-size: 13px;
+      }
+    
+    > span {
+      color: #fff;
+      font-size: 13px;
+      white-space: nowrap;
+
+      &:hover {
+        color: #df3d41;
+        cursor: pointer;
+        transition: 0.3s ease;
+      }
+
+      @media screen and (max-width: 550px){
+        font-size: 10px;
+      }
+    }
+  }
+
+  > div:nth-child(2) {
+    flex-wrap: wrap;
+  }
   display: flex;
   flex-direction: column;
-  gap: 15px;
-  padding: 7% 0;
-  > h4 {
-    pointer-events: none;
-  }
-  >span {
-    font-size:.9rem;
-    cursor: pointer;
+  gap:25px;
+`;
 
-    &:hover {
-      color: #d8d8d8;
-    }
-  }
-  @media screen and (max-width: 1024px){
-    > span {
-      font-size: .8rem;
-    }
-  }
-  @media screen and (max-width: 620px){
-    > span {
-      font-size: .6rem;
-    }
-    > h4 {
-      font-size: .8rem;
-    }
-  }
-`
+export const SFooterContact = styled(SFooterInfo)``;
 
-export const SFooterContact = styled(SFooterInfo)`
+export const SFooterInfoContact = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 
+  @media screen and (max-width: 700px){
+    flex-direction: row;
+  }
 `
