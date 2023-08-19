b3f358c637e44dbf59814af0cda4be6b7b42de4b
vazha2000 <vaja.siqtura2000@gmail.com>
2023-08-19 17:11:41 +0400

    update mobileMenu; update style

--- src/components/MobileMenu/MobileMenu.jsx
+++ src/components/MobileMenu/MobileMenu.jsx
@@ -6,6 +6,7 @@ import {
   SMobileMenuCategoriesList,
   SMobileMenuCategoriesListItems,
   SMobileMenuClose,
+  SMobileMenuNameIcon,
   SMobileMenuTitle,
   SMobileSubcategoriesList,
   SMobileSubcategoriesListItems,
@@ -17,8 +18,8 @@ import { AnimatePresence, motion } from "framer-motion";
 import { SStyledLink } from "../DropdownMenu/DropdownMenu.styled";
 import { v4 as uuidv4 } from "uuid";
 
-export const MobileMenu = ({handleMobileMenuClick}) => {
-  const [isCategoryClicked, setIsCategoryClicked] = useState(false);
+export const MobileMenu = ({ handleMobileMenuClick, isOpen, setIsOpen }) => {
+  // const [isCategoryClicked, setIsCategoryClicked] = useState(false);
   const [openSubcategories, setOpenSubcategories] = useState(
     Array(categoriesListItems.length).fill(false)
   );
@@ -26,78 +27,105 @@ export const MobileMenu = ({handleMobileMenuClick}) => {
   const handleCategoryClick = (index) => {
     const newOpenSubcategories = [...openSubcategories];
     newOpenSubcategories[index] = !newOpenSubcategories[index];
-    setOpenSubcategories(newOpenSubcategories)
+    setOpenSubcategories(newOpenSubcategories);
   };
 
   const mobileMenuVariants = {
     close: {
-      x: "-100%",
-      transition: {stiffness: 0, duration: 0.4}
+      x: "100%",
+      transition: { stiffness: 0, duration: 0.4 },
     },
     open: {
       x: 0,
-      transition: {stiffness: 0, duration: 0.4}
-    }
-  }
+      transition: { stiffness: 0, duration: 0.4 },
+    },
+  };
   const dropdownVariants = {
     hidden: { opacity: 1, height: 0 },
     visible: { opacity: 1, height: "auto", transition: { duration: 0.5 } },
   };
 
   const hiddenVariant = {
-    hidden: {opacity: 1, height: 0}
-  }
+    hidden: { opacity: 1, height: 0 },
+  };
 
   return (
-    <SMobileMenu variants={mobileMenuVariants} initial="close" animate="open" exit="close">
+    <SMobileMenu
+      variants={mobileMenuVariants}
+      initial="close"
+      animate="open"
+      exit="close"
+    >
       <STitleCloseButton>
         <SMobileMenuTitle>კატეგორიები</SMobileMenuTitle>
-        <SMobileMenuClose onClick={handleMobileMenuClick} src="assets/svg/close.svg" alt="close icon" />
+        <SMobileMenuClose
+          onClick={() => setIsOpen(false)}
+          src="assets/svg/close.svg"
+          alt="close icon"
+          width={20}
+        />
       </STitleCloseButton>
       <SMobileMenuCategories>
-        <SMobileMenuCategoriesList
-          onClick={() => setIsCategoryClicked(!isCategoryClicked)}
-        >
-          <span>კატეგორიები</span>
-          <motion.img animate={isCategoryClicked ? {rotateX: 180} : {rotateX: 0}} src="assets/svg/vectorDown.svg" alt="vector down" />
-        </SMobileMenuCategoriesList>
+        {/* <SMobileMenuCategoriesList> */}
+
+        {/* <span>კატეგორიები</span> */}
+        {/* <motion.img animate={isCategoryClicked ? {rotateX: 180} : {rotateX: 0}} src="assets/svg/vectorDown.svg" alt="vector down" /> */}
+        {/* </SMobileMenuCategoriesList> */}
         {/* triggered when isCategoryClicked state changes */}
-          <SMobileMenuCategoriesContainer
-          variants={isCategoryClicked ? dropdownVariants : hiddenVariant}
-          initial={isCategoryClicked ? "hidden" : "hidden"}
-          animate="visible"
-          >
-            {categoriesListItems.map((item, index) => {
-              return (
-                <motion.div key={index}>
-                  <SMobileMenuCategoriesListItems
-                    onClick={() => handleCategoryClick(index)}
-                  >
+        <SMobileMenuCategoriesContainer
+        // variants={isCategoryClicked ? dropdownVariants : hiddenVariant}
+        // initial={isCategoryClicked ? "hidden" : "hidden"}
+        // animate="visible"
+        >
+          {categoriesListItems.map((item, index) => {
+            return (
+              <motion.div key={index}>
+                <SMobileMenuCategoriesListItems
+                  onClick={() => handleCategoryClick(index)}
+                >
+                  <SMobileMenuNameIcon>
+                    <img src={item.svg} alt="" width={20} />
                     <span>{item.name}</span>
-                    <motion.img
-                      animate={openSubcategories[index] ? {rotateX: 180} : {rotateX: 0}}
-                      src="assets/svg/vectorDown.svg"
-                      width={25}
-                      alt="vector down"
-                    />
-                  </SMobileMenuCategoriesListItems>
-                  <AnimatePresence>
+                  </SMobileMenuNameIcon>
+                  <motion.img
+                    animate={
+                      openSubcategories[index]
+                        ? { rotateX: 180 }
+                        : { rotateX: 0 }
+                    }
+                    src="assets/svg/vectorDown.svg"
+                    width={25}
+                    alt="vector down"
+                  />
+                </SMobileMenuCategoriesListItems>
+                <AnimatePresence>
                   {openSubcategories[index] && (
-                    <SMobileSubcategoriesList variants={dropdownVariants} initial="hidden" animate="visible" exit="hidden">
+                    <SMobileSubcategoriesList
+                      variants={dropdownVariants}
+                      initial="hidden"
+                      animate="visible"
+                      exit="hidden"
+                    >
                       {item.subcategories.map((subcategory, subIndex) => (
-                        <SStyledLink to={`/${item.categoryName}/${subcategory.subcategoryName}`}  key={uuidv4()}>
-                          <SMobileSubcategoriesListItems onClick={handleMobileMenuClick} key={subIndex}>
+                        <SStyledLink
+                          to={`/${item.categoryName}/${subcategory.subcategoryName}`}
+                          key={uuidv4()}
+                        >
+                          <SMobileSubcategoriesListItems
+                            onClick={handleMobileMenuClick}
+                            key={subIndex}
+                          >
                             {subcategory.name}
                           </SMobileSubcategoriesListItems>
                         </SStyledLink>
                       ))}
                     </SMobileSubcategoriesList>
                   )}
-                  </AnimatePresence>
-                </motion.div>
-              );
-            })}
-          </SMobileMenuCategoriesContainer>
+                </AnimatePresence>
+              </motion.div>
+            );
+          })}
+        </SMobileMenuCategoriesContainer>
         <SMobileMenuCategoriesList>მთავარი</SMobileMenuCategoriesList>
         <SMobileMenuCategoriesList>ONLINE განვადება</SMobileMenuCategoriesList>
         <SMobileMenuCategoriesList>კონტაქტი</SMobileMenuCategoriesList>
--- src/components/MobileMenu/MobileMenu.styled.js
+++ src/components/MobileMenu/MobileMenu.styled.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
-import { SSideBarCategories, SSideBarCategoriesList } from "../Sidebar/Sidebar.styled";
+import { SSideBarCategories, SSideBarCategoriesIconName, SSideBarCategoriesList } from "../Sidebar/Sidebar.styled";
 import { motion } from "framer-motion";
+import { SCartInfoHeaderContainer } from "../CartInfo/CartInfo.styled";
 
 export const SMobileMenu = styled(motion.div)`
   position: absolute;
@@ -8,27 +9,22 @@ export const SMobileMenu = styled(motion.div)`
   left: 0;
   background-color: #e3e2de;
   height: 150vh;
-  width: 70%;
-  z-index: 2;
+  width: 100%;
+  z-index: 6;
   -webkit-box-shadow: 2px 0px 12px 0px rgba(0,0,0,0.75);
   -moz-box-shadow: 2px 0px 12px 0px rgba(0,0,0,0.75);
   box-shadow: 2px 0px 12px 0px rgba(0,0,0,0.75);
 `
-export const STitleCloseButton = styled.div`
-  display: flex;
-  width: 100%;
-  justify-content: space-between;
-  align-items: center;
-  padding: 1rem .5rem;
-  white-space: nowrap;
+export const STitleCloseButton = styled(SCartInfoHeaderContainer)`
 `
 export const SMobileMenuTitle = styled.h3`
-  color: #df3d41;
+  color: #fff;
   line-height: 1;
 `
 
 export const SMobileMenuClose = styled.img`
   cursor: pointer;
+  filter: brightness(0) invert(1);
 `
 
 export const SMobileMenuCategories = styled(SSideBarCategories)`
@@ -40,16 +36,35 @@ export const SMobileMenuCategoriesList = styled(SSideBarCategoriesList)`
   display: flex;
   align-items: center;
   font-size: 13px;
+  padding: 0 20px;
   text-shadow:1px 1px 2px rgba(0,0,0, 0.15);
-  &:nth-last-child(-n+3) {
+  &:nth-last-child(-n + 3) {
     display: none;
-    @media screen and (max-width: 768px){
+  }
+  @media screen and (max-width: 900px) {
+    &:nth-last-child(2) {
       display: block;
     }
   }
+  @media screen and (max-width: 550px) {
+    &:nth-last-child(1) {
+      display: block;
+    }
+  }
+  @media screen and (max-width: 420px) {
+    &:nth-last-child(3) {
+      display: block;
+    }
+  }
+  
 `
 export const SMobileMenuCategoriesContainer = styled(motion.ul)`
   overflow: hidden;
+  > li:nth-child(2) {
+    background-color: red;
+  }
+  display: flex;
+  flex-direction: column;
 `
 
 export const SMobileMenuCategoriesListItems = styled.li`
@@ -58,23 +73,54 @@ export const SMobileMenuCategoriesListItems = styled.li`
   width: 100%;
   justify-content: space-between;
   align-items: center;
+  padding: 0 30px;
+  height: 50px;
+  border-bottom: 1px solid rgba(0,0,0, 0.15);
   &:hover {
-    background-color: aquamarine;
     cursor: pointer;
     background-color: rgba(223, 61, 65, 0.4);
     color: #f2f2f2;
+    > div > img {
+      filter: brightness(0) invert(1);
+    }
+    > img {
+      filter: brightness(0) invert(1);
+    }
+  }
+
+`
+
+export const SMobileMenuNameIcon = styled.div`
+  position: relative;
+
+  > span {
+    position: absolute;
+    top: 5px;
+    left: 30px;
+    white-space: nowrap;
   }
 `
 
 export const SMobileSubcategoriesList = styled(motion.ul)`
-  padding: 0 .8rem;
   overflow: hidden;
+  width: 90%;
+  margin: 0 auto;
+
+  > li:nth-last-child(2) {
+    border-bottom: none;
+  }
 `
 
 export const SMobileSubcategoriesListItems = styled.li`
-  font-size: 13px;
+  font-size: 12px;
   cursor: pointer;
   &:hover {
     background-color: rgba(223, 61, 65, 0.6);
+    color: #fff;
   }
+  width: 100%;
+  height: 30px;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid rgba(0,0,0, 0.15);
 `
