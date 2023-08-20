dd05fb17ed7e997f478f73d8aecff76d450f7a7f
vazha2000 <vaja.siqtura2000@gmail.com>
2023-08-20 20:32:16 +0400

    add product images components; add next and previous functionalities

--- src/components/ProductImages/ProductImages.jsx
+++ src/components/ProductImages/ProductImages.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { SNextArrow, SPrevArrow, SProductImage, SProductImageContainer, SProductImages } from './ProductImages.styled'
 
-export const ProductImages = () => {
+export const ProductImages = ({productImage}) => {
+  const [imageIndex, setImageIndex] = useState(0);
+
+  const handleNext = () => {
+    setImageIndex((prevIndex) => (prevIndex + 1) % productImage.length);
+  };
+
+  const handlePrev = () => {
+    setImageIndex((prevIndex) => (prevIndex - 1 + productImage.length) % productImage.length);
+  };
+  
   return (
-    <div>ProductImages</div>
+    <SProductImages>
+      <SProductImageContainer>
+        <SProductImage src={productImage[imageIndex]}/>
+        <SPrevArrow onClick={handlePrev}>-</SPrevArrow>
+        <SNextArrow onClick={handleNext}>+</SNextArrow>
+      </SProductImageContainer>
+    </SProductImages>
   )
 }
