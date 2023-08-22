c06b4d6a580992cdad68896cb11d99770a9786cb
vazha2000 <vaja.siqtura2000@gmail.com>
2023-08-22 22:42:57 +0400

    add Searched products folder with its file structure

---
+++ src/views/SearchedProducts/SearchedProducts.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const SearchedProducts = () => {
+  return (
+    <div>SearchedProducts</div>
+  )
+}
---
+++ src/views/SearchedProducts/index.js
@@ -0,0 +1,1 @@
+export { SearchedProducts } from "./SearchedProducts"
