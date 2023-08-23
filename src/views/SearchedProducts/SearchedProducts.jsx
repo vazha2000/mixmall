af1dca6c6ae68f0feeaaac6bbb10512208522a42
vazha2000 <vaja.siqtura2000@gmail.com>
2023-08-23 14:25:20 +0400

    render pagination conditionally

--- src/views/SearchedProducts/SearchedProducts.jsx
+++ src/views/SearchedProducts/SearchedProducts.jsx
@@ -1,7 +1,8 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { AllFoundProductsContext } from "../../context/AllFoundProductsContext";
 import {
   SSearchedProductsContainer,
+  SSearchedProductsNoData,
   SSearchedProductsPagination,
   SSearchedProductsPaginationButton,
   SSearchedProductsPaginationList,
@@ -23,6 +24,8 @@ export const SearchedProducts = () => {
     allFoundProducts.map(() => false)
   );
 
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
   const handleHover = (index) => {
     setHoverStates((prevStates) => {
       const updatedStates = [...prevStates];
@@ -36,7 +39,20 @@ export const SearchedProducts = () => {
   //     setCurrentPage(pageNumber);
   //   }
   // };
-  const maxPageNumbersToShow = 5;
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const maxPageNumbersToShow = windowWidth < 450 ? 3 : 5;
 
   const pageNumbers = Array.from(
     { length: totalPages },
@@ -52,94 +68,119 @@ export const SearchedProducts = () => {
     const rightEllipsis = currentPage + Math.floor(maxPageNumbersToShow / 3);
 
     if (leftEllipsis <= 1) {
-      return [...pageNumbers.slice(0, maxPageNumbersToShow - 1), "ellipsis", totalPages];
+      return [
+        ...pageNumbers.slice(0, maxPageNumbersToShow - 1),
+        "ellipsis",
+        totalPages,
+      ];
     } else if (rightEllipsis >= totalPages) {
-      return [1, "ellipsis", ...pageNumbers.slice(totalPages - maxPageNumbersToShow + 2)];
+      return [
+        1,
+        "ellipsis",
+        ...pageNumbers.slice(totalPages - maxPageNumbersToShow + 2),
+      ];
     } else {
-      return [1, "ellipsis", ...pageNumbers.slice(leftEllipsis, rightEllipsis), "ellipsis", totalPages];
+      return [
+        1,
+        "ellipsis",
+        ...pageNumbers.slice(leftEllipsis, rightEllipsis),
+        "ellipsis",
+        totalPages,
+      ];
     }
   };
 
   const handleNextPage = () => {
-    if(currentPage !== totalPages) {
-      setCurrentPage(prev => prev + 1)
+    if (currentPage !== totalPages) {
+      setCurrentPage((prev) => prev + 1);
     }
-  }
+  };
 
   const handlePrevPage = () => {
-    if(currentPage !== 1) {
-      setCurrentPage(prev => prev - 1)
+    if (currentPage !== 1) {
+      setCurrentPage((prev) => prev - 1);
     }
-  }
+  };
 
   const handlePageChange = (pageNumber) => {
     setCurrentPage(pageNumber);
   };
 
-
   return (
-    <SSearchedProductsContainer>
-      {currentProducts.map(
-        (
-          [
-            productName,
-            categoryName,
-            productId,
-            oldPrice,
-            currentPrice,
-            productImage,
-            isDiscount,
-            discountRate,
-            alt,
-          ],
-          index
-        ) => (
-          <div key={index}>
-            <Card
-              key={index}
-              index={index}
-              width={"200px"}
-              productImage={productImage}
-              discountRate={discountRate}
-              isDiscount={isDiscount}
-              productName={productName}
-              currentPrice={currentPrice}
-              oldPrice={oldPrice}
-              alt={alt}
-              isHovered={hoverStates[index]}
-              handleHover={() => handleHover(index)}
-              path={`/${categoryName}/${productId}/${productName}`}
-            />
-          </div>
-        )
+    <div>
+      <SSearchedProductsContainer>
+        {currentProducts.map(
+          (
+            [
+              productName,
+              categoryName,
+              productId,
+              oldPrice,
+              currentPrice,
+              productImage,
+              isDiscount,
+              discountRate,
+              alt,
+            ],
+            index
+          ) => (
+            <div key={index}>
+              <Card
+                key={index}
+                index={index}
+                width={"200px"}
+                productImage={productImage}
+                discountRate={discountRate}
+                isDiscount={isDiscount}
+                productName={productName}
+                currentPrice={currentPrice}
+                oldPrice={oldPrice}
+                alt={alt}
+                isHovered={hoverStates[index]}
+                handleHover={() => handleHover(index)}
+                path={`/${categoryName}/${productId}/${productName}`}
+              />
+            </div>
+          )
+        )}
+      </SSearchedProductsContainer>
+      {currentProducts.length == 0 ? (
+        <SSearchedProductsNoData>
+          <h1>პროდუქტების სია ცარიელია</h1>
+        </SSearchedProductsNoData>
+      ) : (
+        <SSearchedProductsPagination>
+          <SSearchedProductsPaginationList>
+            <SSearchedProductsPaginationButton onClick={handlePrevPage}>
+              <img src="assets/svg/paginationLeft.svg" alt="left" />
+            </SSearchedProductsPaginationButton>
+            {getPageNumbersWithEllipsis().map((page, index) => (
+              <React.Fragment key={index}>
+                {page === "ellipsis" ? (
+                  <SPaginationEllipsis>
+                    <img src="assets/svg/ellipsis.svg" alt="ellipsis" />
+                  </SPaginationEllipsis>
+                ) : (
+                  <SSearchedProductsPaginationListItems
+                    onClick={() => handlePageChange(page)}
+                    isActive={currentPage === page}
+                  >
+                    {page}
+                  </SSearchedProductsPaginationListItems>
+                )}
+              </React.Fragment>
+            ))}
+            <SSearchedProductsPaginationButton
+              onClick={handleNextPage}
+              rotate="true"
+              lastPage={currentPage === totalPages}
+            >
+              <img src="assets/svg/paginationLeft.svg" alt="right" />
+            </SSearchedProductsPaginationButton>
+          </SSearchedProductsPaginationList>
+        </SSearchedProductsPagination>
       )}
-      <SSearchedProductsPagination>
-        <SSearchedProductsPaginationList>
-          <SSearchedProductsPaginationButton onClick={handlePrevPage}>
-            <img src="assets/svg/paginationLeft.svg" alt="left" />
-          </SSearchedProductsPaginationButton>
-          {getPageNumbersWithEllipsis().map((page, index) => (
-          <React.Fragment key={index}>
-            {page === "ellipsis" ? (
-              <SPaginationEllipsis>
-                <img src="assets/svg/ellipsis.svg" alt="ellipsis" />
-              </SPaginationEllipsis>
-            ) : (
-              <SSearchedProductsPaginationListItems
-                onClick={() => handlePageChange(page)}
-                isActive={currentPage === page}
-              >
-                {page}
-              </SSearchedProductsPaginationListItems>
-            )}
-          </React.Fragment>
-        ))}
-        <SSearchedProductsPaginationButton onClick={handleNextPage} rotate="true" lastPage={currentPage === totalPages}>
-          <img src="assets/svg/paginationLeft.svg" alt="right" />
-        </SSearchedProductsPaginationButton>
-        </SSearchedProductsPaginationList>
-      </SSearchedProductsPagination>
-    </SSearchedProductsContainer>
+    </div>
   );
 };
 
