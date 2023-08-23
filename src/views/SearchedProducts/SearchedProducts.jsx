a1bb9bd56cdb5ab80e51642fb5319dbc2ccbb644
vazha2000 <vaja.siqtura2000@gmail.com>
2023-08-23 12:58:34 +0400

    display cards

--- src/views/SearchedProducts/SearchedProducts.jsx
+++ src/views/SearchedProducts/SearchedProducts.jsx
@@ -1,7 +1,65 @@
-import React from 'react'
-
+import React, { useContext, useState } from "react";
+import { AllFoundProductsContext } from "../../context/AllFoundProductsContext";
+import { SSearchedProductsContainer } from "./SearchedProducts.styled";
+import { Card } from "../../components/Card";
 export const SearchedProducts = () => {
+  const { allFoundProducts, currentPage, setCurrentPage, productsPerPage } =
+    useContext(AllFoundProductsContext);
+
+  const totalPages = Math.ceil(allFoundProducts.length / productsPerPage);
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const [hoverStates, setHoverStates] = useState(allFoundProducts.map(() => false));
+
+
+  const handleHover = (index) => {
+    setHoverStates((prevStates) => {
+      const updatedStates = [...prevStates];
+      updatedStates[index] = !updatedStates[index];
+      return updatedStates;
+    });
+  };
+
+  console.log(allFoundProducts)
   return (
-    <div>SearchedProducts</div>
-  )
-}
+    <SSearchedProductsContainer>
+      {allFoundProducts.map(
+        (
+          [
+            productName,
+            categoryName,
+            productId,
+            oldPrice,
+            currentPrice,
+            productImage,
+            isDiscount,
+            discountRate,
+            alt
+          ],
+          index
+        ) => (
+          <div key={index}>
+            <Card
+              key={index}
+              index={index}
+              width={"200px"}
+              productImage={productImage}
+              discountRate={discountRate}
+              isDiscount={isDiscount}
+              productName={productName}
+              currentPrice={currentPrice}
+              oldPrice={oldPrice}
+              alt={alt}
+              isHovered={hoverStates[index]}
+              handleHover={() => handleHover(index)}
+              path={`/${categoryName}/${productId}/${productName}`}
+            />
+          </div>
+        )
+      )}
+    </SSearchedProductsContainer>
+  );
+};
