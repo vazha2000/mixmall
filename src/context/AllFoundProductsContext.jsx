0c64423ba1711b3ce9732bf4c31cbaeda770d60d
vazha2000 <vaja.siqtura2000@gmail.com>
2023-08-23 13:38:08 +0400

    update context for pagination

--- src/context/AllFoundProductsContext.jsx
+++ src/context/AllFoundProductsContext.jsx
@@ -4,10 +4,26 @@ const AllFoundProductsContext = createContext();
 
 const AllFoundProductsProvider = ({ children }) => {
   const [allFoundProducts, setAllFoundProducts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const productsPerPage = 8;
 
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentProducts = allFoundProducts.slice(
+    indexOfFirstProduct,
+    indexOfLastProduct
+  );
+  
   return (
     <AllFoundProductsContext.Provider
-      value={{ allFoundProducts, setAllFoundProducts }}
+      value={{
+      allFoundProducts,
+      currentProducts,
+      setAllFoundProducts,
+      currentPage,
+      setCurrentPage,
+      productsPerPage,
+    }}
     >
       {children}
     </AllFoundProductsContext.Provider>
