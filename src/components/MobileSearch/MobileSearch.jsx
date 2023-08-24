09cd70f301f4babb5b607c18cac9212f323b5fd2
vazha2000 <vaja.siqtura2000@gmail.com>
2023-08-24 14:45:45 +0400

    add components in mobile search

--- src/components/MobileSearch/MobileSearch.jsx
+++ src/components/MobileSearch/MobileSearch.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import {
+  SMobileSearch,
+  SmobileSearchHeaderContainer,
+} from "./MobileSearch.styled";
+import { SearchInput } from "../SearchInput";
 
-export const MobileSearch = () => {
+export const MobileSearch = ({
+  isMobileSearchClicked,
+  setIsMobileSearchClicked,
+  windowWidth,
+}) => {
+  useEffect(() => {
+    if (windowWidth > 576) {
+      setIsMobileSearchClicked(false);
+    }
+  }, [windowWidth, setIsMobileSearchClicked]);
   return (
-    <div>MobileSearch</div>
-  )
-}
+    <SMobileSearch
+      initial={{ x: "100%" }}
+      animate={{ x: 0 }}
+      exit={{ x: "100%" }}
+      transition={{ type: "tween", duration: 0.4, delay: 0.1 }}
+    >
+      <SmobileSearchHeaderContainer>
+        <SearchInput isMobileSearchClicked={isMobileSearchClicked} setIsMobileSearchClicked={setIsMobileSearchClicked} height={"30%"} />
+      </SmobileSearchHeaderContainer>
+    </SMobileSearch>
+  );
+};
---
+++ src/components/MobileSearch/MobileSearch.styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import { SCartInfo, SCartInfoHeaderContainer } from "../CartInfo/CartInfo.styled";
+
+export const SMobileSearch = styled(SCartInfo)`
+  width: 100%;
+  border: none;
+`
+
+export const SmobileSearchHeaderContainer = styled(SCartInfoHeaderContainer)`
+  height: 80px;
+`
