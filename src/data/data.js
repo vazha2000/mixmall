003903237975afa359b6a284aea3f02239b37a35
vazha2000 <vaja.siqtura2000@gmail.com>
2023-09-03 15:20:25 +0400

    update data

--- src/data/data.js
+++ src/data/data.js
@@ -25,7 +25,7 @@ export const categoriesListItems = [
   {
     id: 2,
     name: "მსხვილი საყოფაცხოვრებო ტექნიკა",
-    categoryName: "major&appliance",
+    categoryName: "მსხვილი საყოფაცხოვრებო ტექნიკა",
     subcategories: [...majorAppliance],
     svg: "assets/svg/CategoriesIcons/washingmachine.svg",
     alt: "washingmachine",
@@ -114,46 +114,136 @@ export const categoriesListItems = [
 
 // console.log(categoriesListItems[0])
 
-export const testProducts = [
+export const saleProducts = [
   {
     id: 1,
-    productImage: ["../assets/images/computerTechnic/orange.png", "../assets/images/computerTechnic/orange.png", "../assets/images/computerTechnic/orange.png", "../assets/images/computerTechnic/orange.png", "../assets/images/computerTechnic/orange.png"],
+    productImage: ["../assets/images/მსხვილი-საყოფაცხოვრებო-ტექნიკა/ჩასაშენებელი სამეული/ჩასაშენებელი-სამეული-ALNEO-BLACK/AlneoMain.webp","../assets/images/მსხვილი-საყოფაცხოვრებო-ტექნიკა/ჩასაშენებელი სამეული/ჩასაშენებელი-სამეული-ALNEO-BLACK/Alneo1.webp", "../assets/images/მსხვილი-საყოფაცხოვრებო-ტექნიკა/ჩასაშენებელი სამეული/ჩასაშენებელი-სამეული-ALNEO-BLACK/Alneo2.webp", "../assets/images/მსხვილი-საყოფაცხოვრებო-ტექნიკა/ჩასაშენებელი სამეული/ჩასაშენებელი-სამეული-ALNEO-BLACK/Alneo3.webp"],
     discountRate: "20%",
     isDiscount: false,
-    productName: "მანქანის კატალიზატორი და კიდევ მრავალი სხვა",
-    currentPrice: 260,
-    oldPrice: 300,
-    alt: "test",
+    productName: "ჩასაშენებელი სამეული ALNEO BLACK",
+    currentPrice: 760,
+    oldPrice: 600,
+    warranty: "3 წელი",
+    alt: "Alneo",
+    isInWishlist: false,
+    categoryName: "მსხვილი საყოფაცხოვრებო ტექნიკა",
+    subcategoryName: "ჩასაშენებელი სამეული",
+    descriptions: [
+      {
+        "გამწოვი":"ALNEO T9500 BLACK (01464) /60*30*13/",
+        "პერფორმანსი": "600მ³",
+        "გამწოვის ზომა": "60*30*13",
+        "გაზქურის ზედაპირი:":"Alneo6064GB-(01552)",
+        "გაზქურის ზომა": "60*51",
+        "მინა": "დიახ",
+        "თუჯის დასადგმელი":"დიახ",
+        "გაზკონტროლი": "დიახ",
+        "ჩასაშენებელი ღუმელი":"ALNEO A5010T (01633) ემალის დაფარვით",
+        "პროგრამა": "6",
+        "მექაინუკური მართვა": "დიახ"
+      }
+    ]
+    
   },
   {
     id: 2,
-    productImage: ["../assets/images/computerTechnic/orange.png", "../assets/images/computerTechnic/orange.png", "../assets/images/computerTechnic/orange.png", "../assets/images/computerTechnic/orange.png", "../assets/images/computerTechnic/orange.png"],
-    discountRate: "40%",
-    isDiscount: true,
-    productName: "მიკრო ტალღური ღუმელი",
-    currentPrice: 260,
-    oldPrice: 300,
-    alt: "test",
+    productImage: ["../assets/images/მსხვილი-საყოფაცხოვრებო-ტექნიკა/ჩასაშენებელი სამეული/ჩასაშენებელი-სამეული-ALNEO-INOX-V1/AlneoInoxMain.webp","../assets/images/მსხვილი-საყოფაცხოვრებო-ტექნიკა/ჩასაშენებელი სამეული/ჩასაშენებელი-სამეული-ALNEO-INOX-V1/AlneoInox1.webp", "../assets/images/მსხვილი-საყოფაცხოვრებო-ტექნიკა/ჩასაშენებელი სამეული/ჩასაშენებელი-სამეული-ALNEO-INOX-V1/AlneoInox2.webp","../assets/images/მსხვილი-საყოფაცხოვრებო-ტექნიკა/ჩასაშენებელი სამეული/ჩასაშენებელი-სამეული-ALNEO-INOX-V1/AlneoInox3.webp",],
+    discountRate: "20%",
+    isDiscount: false,
+    productName: "ჩასაშენებელი სამეული ALNEO INOX V1",
+    currentPrice: 749,
+    oldPrice: 600,
+    warranty: "3 წელი",
+    alt: "Alneo",
+    isInWishlist: false,
+    categoryName: "მსხვილი საყოფაცხოვრებო ტექნიკა",
+    subcategoryName: "ჩასაშენებელი სამეული",
+    descriptions: [
+      {
+        "გამწოვი":"ALNEO ASLCF-2 INOX (01463)",
+        "პერფორმანსი": "600მ³",
+        "გამწოვის ზომა": "60*30*13",
+        "გაზქურის ზედაპირი:":"Alneo6064 INOX-(01551)",
+        "გაზქურის ზომა": "60*51",
+        "უჟანგავი მეტალი": "დიახ",
+        "თუჯის დასადგმელი": "დიახ",
+        "გაზკონტროლი":"დიახ",
+        "ფერი": "ვერცხლისფერი",
+        "ჩასაშენებელი ღუმელი":"ALNEO A5010 inox (01634) ინოქსი", 
+        "პროგრამა": "6",
+        "მექანიკური მართვა":"დიახ"
+      }
+    ]
+    
   },
   {
     id: 3,
-    productImage: ["../assets/images/computerTechnic/orange.png", "../assets/images/computerTechnic/orange.png", "../assets/images/computerTechnic/orange.png", "../assets/images/computerTechnic/orange.png", "../assets/images/computerTechnic/orange.png"],
-    discountRate: "10%",
+    productImage: ["../assets/images/მსხვილი-საყოფაცხოვრებო-ტექნიკა/ჩასაშენებელი სამეული/ჩასაშენებელი-სამეული-ALNEO-INOX-V2/AlneoInoxV2Main.webp","../assets/images/მსხვილი-საყოფაცხოვრებო-ტექნიკა/ჩასაშენებელი სამეული/ჩასაშენებელი-სამეული-ALNEO-INOX-V2/AlneoInoxV2-1.webp", "../assets/images/მსხვილი-საყოფაცხოვრებო-ტექნიკა/ჩასაშენებელი სამეული/ჩასაშენებელი-სამეული-ALNEO-INOX-V2/AlneoInoxV2-2.webp", "../assets/images/მსხვილი-საყოფაცხოვრებო-ტექნიკა/ჩასაშენებელი სამეული/ჩასაშენებელი-სამეული-ALNEO-INOX-V2/AlneoInoxV2-3.webp"],
+    discountRate: "20%",
     isDiscount: false,
-    productName: "გეიმინგ მონიტორის დამჭერი ",
-    currentPrice: 260,
-    oldPrice: 300,
-    alt: "test",
+    productName: "ჩასაშენებელი სამეული ALNEO INOX V2",
+    currentPrice: 749,
+    oldPrice: 600,
+    warranty: "3 წელი",
+    alt: "Alneo",
+    isInWishlist: false,
+    categoryName: "მსხვილი საყოფაცხოვრებო ტექნიკა",
+    subcategoryName: "ჩასაშენებელი სამეული",
+    descriptions: [
+      {
+        "გამწოვი":"ALNEO T9500 Inox (01465)",
+        "პერფორმანსი": "600მ³",
+        "გამწოვის ზომა": "60*30*13",
+        "გამწოვის ფერი": "ვერცხლისფერი",
+        "გაზქურის ზედაპირი:":"Alneo6064 INOX-(01551)",
+        "გაზქურის ზომა": "60*51",
+        "უჟანგავი მეტალი": "დიახ",
+        "თუჯის დასადგმელი": "დიახ",
+        "გაზკონტროლი":"დიახ",
+        "ფერი": "ვერცხლისფერი",
+        "ჩასაშენებელი ღუმელი":"ALNEO A5010 inox (01634) ინოქსი", 
+        "პროგრამა": "6",
+        "მექანიკური მართვა":"დიახ"
+      }
+    ]
+    
   },
   {
     id: 4,
-    productImage: ["../assets/images/computerTechnic/orange.png", "../assets/images/computerTechnic/orange.png", "../assets/images/computerTechnic/orange.png", "../assets/images/computerTechnic/orange.png", "../assets/images/computerTechnic/orange.png"],
-    discountRate: "900%",
+    productImage: ["../assets/images/მსხვილი-საყოფაცხოვრებო-ტექნიკა/ჩასაშენებელი სამეული/ჩასაშენებელი-სამეული-SILVERLINE-INOX/SilverlineInoxMain.webp","../assets/images/მსხვილი-საყოფაცხოვრებო-ტექნიკა/ჩასაშენებელი სამეული/ჩასაშენებელი-სამეული-SILVERLINE-INOX/SilverlineInox1.webp", "../assets/images/მსხვილი-საყოფაცხოვრებო-ტექნიკა/ჩასაშენებელი სამეული/ჩასაშენებელი-სამეული-SILVERLINE-INOX/SilverlineInox2.webp", "../assets/images/მსხვილი-საყოფაცხოვრებო-ტექნიკა/ჩასაშენებელი სამეული/ჩასაშენებელი-სამეული-SILVERLINE-INOX/SilverlineInox3.webp"],
+    discountRate: "20%",
     isDiscount: false,
-    productName: "გასაბერი აუზი",
-    currentPrice: 260,
-    oldPrice: 300,
-    alt: "test",
+    productName: "ჩასაშენებელი-სამეული-SILVERLINE-INOX",
+    currentPrice: 1179,
+    oldPrice: 600,
+    warranty: "3 წელი",
+    alt: "Silverline",
+    isInWishlist: false,
+    categoryName: "მსხვილი საყოფაცხოვრებო ტექნიკა",
+    subcategoryName: "ჩასაშენებელი სამეული",
+    descriptions: [
+      {
+        "გამწოვი":"Silverline 1144.5 Inox(03495)",
+        "პერფორმანსი": "1000მ³",
+        "გამწოვის ზომა": "60*30*21.5",
+        "გამწოვის ფერი": "ვერცხლისფერი",
+        "გაზქურის ზედაპირი:":"Silverline as5472x02(04018)ინოქსი",
+        "სამი გაზი + 1 დენი": "დიახ",
+        "ორმაგი წვა": "დიახ",
+        "თუჯის სადგამი": "დიახ",
+        "გაზკონტროლი": "დიახ",
+        "ჩასაშენებელი ღუმელი":"Silverline BO6502X02 (045228)", 
+        "პროგრამა": "6",
+        "ერთდროული წვა":"დიახ",
+        "კონვექცია": "დიახ",
+        "ღუმელის ზომა": "60სმ",
+        "გალღობა": "დიახ",
+        "ტაიმერი": "დიახ",
+        "გრილი": "დიახ",
+        "ფერი": "ვერცხლისფერი",
+      }
+    ]
+    
   },
   {
     id: 5,
