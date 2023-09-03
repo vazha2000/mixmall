f6a813d28fa937e584837d7f8d08e590d815a06c
vazha2000 <vaja.siqtura2000@gmail.com>
2023-09-03 19:26:56 +0400

    add path; add onclick functionality to scroll up when button is clicked

--- src/components/BestSellingProducts/BestSellingProducts.jsx
+++ src/components/BestSellingProducts/BestSellingProducts.jsx
@@ -1,19 +1,29 @@
-import React, { useState } from 'react'
-import { SBestSellingProducts, SBestSellingProductsButton, SBestSellingProductsButtonWrapper, SBestSellingProductsHeader, SBestSellingProductsItems, SBestSellingProductsLabel, SBestSellingProductsLabelText } from './BestSellingProducts.styled'
-import { bestSellingProducts } from '../../data/data'
-import { Card } from '../Card'
+import React, { useState } from "react";
+import {
+  SBestSellingProducts,
+  SBestSellingProductsButton,
+  SBestSellingProductsButtonWrapper,
+  SBestSellingProductsHeader,
+  SBestSellingProductsItems,
+  SBestSellingProductsLabel,
+  SBestSellingProductsLabelText,
+} from "./BestSellingProducts.styled";
+import { bestSellingProducts } from "../../data/data";
+import { Card } from "../Card";
+import { Link } from "react-router-dom";
 
 export const BestSellingProducts = () => {
-
-  const [hoverStates, setHoverStates] = useState(bestSellingProducts.map(() => false));
+  const [hoverStates, setHoverStates] = useState(
+    bestSellingProducts.map(() => false)
+  );
 
   const handleHover = (index) => {
     setHoverStates((prevStates) => {
       const updatedStates = [...prevStates];
       updatedStates[index] = !updatedStates[index];
-      return updatedStates
-    })
-  }
+      return updatedStates;
+    });
+  };
 
   const visibleProducts = bestSellingProducts.slice(0, 8);
 
@@ -21,31 +31,36 @@ export const BestSellingProducts = () => {
     <SBestSellingProducts>
       <SBestSellingProductsLabel>
         <img src="assets/svg/labelIcon.svg" alt="label icon" />
-        <SBestSellingProductsLabelText>საუკეთესოდ გაყიდვადი პროდუქცია</SBestSellingProductsLabelText>
+        <SBestSellingProductsLabelText>
+          საუკეთესოდ გაყიდვადი პროდუქცია
+        </SBestSellingProductsLabelText>
       </SBestSellingProductsLabel>
       <SBestSellingProductsItems>
-      {visibleProducts.map((item, index) => {
-          return <Card
-          key={index}
-          index={index}
-          productImage={item.productImage}
-          discountRate={item.discountRate}
-          isDiscount={item.isDiscount}
-          productName={item.productName}
-          currentPrice={item.currentPrice}
-          oldPrice={item.oldPrice}
-          alt={item.alt}
-          handleHover={() => handleHover(index)}
-          isHovered={hoverStates[index]}
-          path={`/${item.categoryName}/${item.subcategoryName}/${item.productName}`}
-          id={item.id}
-        />
+        {visibleProducts.map((item, index) => {
+          return (
+            <Card
+              key={index}
+              index={index}
+              productImage={item.productImage}
+              discountRate={item.discountRate}
+              isDiscount={item.isDiscount}
+              productName={item.productName}
+              currentPrice={item.currentPrice}
+              oldPrice={item.oldPrice}
+              alt={item.alt}
+              handleHover={() => handleHover(index)}
+              isHovered={hoverStates[index]}
+              path={`/${item.categoryName}/${item.subcategoryName}/${item.productName}`}
+              id={item.id}
+            />
+          );
         })}
       </SBestSellingProductsItems>
       <SBestSellingProductsButtonWrapper>
-      <SBestSellingProductsButton>ყველას ნახვა</SBestSellingProductsButton>
-
+        <Link to="/selling-products">
+          <SBestSellingProductsButton onClick={() => window.scrollTo({top: 0, left: 0})}>ყველას ნახვა</SBestSellingProductsButton>
+        </Link>
       </SBestSellingProductsButtonWrapper>
     </SBestSellingProducts>
-  )
-}
+  );
+};
